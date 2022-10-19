b6b1a4972488a17474ff13f109e9021d2be6d5f0
MattVwaves <jamesmattholt@gmail.com>
2022-10-19 15:19:17 +0100

    presented most of the correct data for different dogs

--- src/index.js
+++ src/index.js
@@ -14,6 +14,32 @@ data.forEach((dogJSData) => {
   li.setAttribute('class', 'dogs-list__button')
   li.innerText = dogJSData.name
 
+  dogImg = document.createElement('img')
+  bio = document.createElement('h2')
+  desc = document.createElement('p')
+  goodOrNot = document.createElement('p')
+  
+  li.addEventListener('click', () => {
+    
+    const dogCard = document.querySelector('.main__dog-section')
+    
+    const dogName = document.querySelector('h2')
+    dogName.innerText = dogJSData.name
+
+    dogImg.src = dogJSData.image
+    dogCard.appendChild(dogImg)
+
+    bio.innerText = 'Bio'
+    dogCard.appendChild(bio)
+
+    desc.innerText = dogJSData.bio
+    dogCard.appendChild(desc)
+
+    goodOrNot.innerText = 'Is naughty?'
+    dogCard.appendChild(goodOrNot)
+
+
+  })
   
   // now add an event listener to listen for user clicks
   // that event listener function, if defined here, will have access to the specific dogJSData for the current forEach iteration
