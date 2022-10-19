341bf7fdad7d0dbfc23242793470d5135900b340
MattVwaves <jamesmattholt@gmail.com>
2022-10-19 13:06:00 +0100

    got buttons and names displayed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 console.log(data);
 
-// WRITE YOUR CODE BELOW!
+// select the dogs-list UL
+const dogsList = document.querySelector('.dogs-list')
+
+
+console.log(dogsList);
+// for each dog JS object in data, generate a li
+data.forEach((dogJSData) => {
+  console.log(dogJSData);
+  // generate this HTML template: <li class="dogs-list__button">Mr. Bonkers</li>
+  const li = document.createElement('li')
+  dogsList.appendChild(li)
+  li.setAttribute('class', 'dogs-list__button')
+  li.innerText = dogJSData.name
+
+  
+  // now add an event listener to listen for user clicks
+  // that event listener function, if defined here, will have access to the specific dogJSData for the current forEach iteration
+});
