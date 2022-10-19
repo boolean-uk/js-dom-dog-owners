e4551b3153bd00594fede07733e0f85d9846a35f
Dave <Daverevell491@gmail.com>
2022-10-19 16:30:32 +0100

    Added card images

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 
 // select the dogs-list UL
 const dogsList = document.querySelector('.dogs-list')
-// console.log(dogsList);
+const dogCard = document.querySelector('.main__dog-section')
+
 
 function dogListButton(index) {
   // generate this HTML template: <li class="dogs-list__button">Mr. Bonkers</li>
@@ -15,12 +16,27 @@ function dogListButton(index) {
   dogListButton.setAttribute('class', 'dogs-list__button') // Give dogListButton the class dogs-list__button
 
   dogListButton.innerText = data[index].name // Set dogListButton's text to be the object key 'name' from within the data array, based on the idex given by the for loop on below
-
+  
   // now add an event listener to listen for user clicks
   dogListButton.addEventListener('click', (event) => {
     // whenever there is a click on the dogListButton trigger the event function
-    console.log(data[index].name) // log the dog's name
+    dogCardName(index)
+    dogCardImg(index)
   })
+}
+
+const dogName = document.querySelector('h2')
+dogName.setAttribute('class', 'dog-name')
+function dogCardName(index) {
+  dogName.innerText = data[index].name
+}
+
+const dogImg = document.createElement('img')
+
+function dogCardImg(index) {
+  dogImg.setAttribute('src', data[index].image)
+  dogImg.setAttribute ('style', 'height: 300px')
+  dogCard.appendChild(dogImg)
 }
 
 // for each dog JS object in data, generate a li
@@ -28,3 +44,4 @@ for (let i = 0; i < data.length; i++) {
   // i = index that is used in line 10, 14 and 16
   dogListButton(i)
 }
+
