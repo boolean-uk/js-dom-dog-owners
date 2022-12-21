9cade570133376c65e05e69078aec10d4480b5f4
benji <benjipictor@gmail.com>
2022-12-21 16:52:28 +0000

    formatting

--- src/index.js
+++ src/index.js
@@ -3,33 +3,48 @@ console.log(data);
 // WRITE YOUR CODE BELOW!
 const doglist = document.querySelector('ul')
 const dogcard = document.querySelector('.main__dog-section')
+const div1 = document.querySelector('.main__dog-section__desc')
 
 for (let i = 0; i < data.length; i++) {
-    const name = data[i].name
+  const name = data[i].name
   const dognames = document.createElement('li')
   dognames.setAttribute('class', 'dogs-list__button')
   doglist.append(dognames)
   dognames.innerText = name
 }
 function dog_1(index) {
-    const div1 = document.createElement('div')
-    div1.setAttribute('class', '.main__dog-section__desc')
-    div1
-   const name1 = document.querySelector('h2')
-   name1.innerText = data[index].name
-   dogcard.append(name1)
-   const img1 = document.createElement('img')
-   img1.setAttribute('src', data[index].image)
-   dogcard.append(img1)
-   const bio = document.createElement('h3')
-   bio.innerText = 'bio'
-   const para = document.createElement('p')
-   para.innerText = data[index].bio
-   dogcard.append(div1)
-   div1.append(bio)
-   div1.append(para)
+  
+  const name1 = document.querySelector('h2')
+  name1.innerText = data[index].name
+  dogcard.append(name1)
 
-   const isGoodDog = document.createElement('Boolean')
+  const img1 = document.createElement('img')
+  img1.setAttribute('src', data[index].image)
+  dogcard.append(img1)
+
+  const div1 = document.createElement('div')
+  div1.setAttribute('class', 'main__dog-section__desc')
+  dogcard.append(div1)
+  
+  const bio = document.createElement('h3')
+  bio.innerText = 'bio'
+  div1.append(bio)
+  
+  const para = document.createElement('p')
+  div1.append(para)
+  para.innerText = data[index].bio
+  
+  const para2 = document.createElement('p')
+  dogcard.append(para2)
+  
+  if (data[index].isGoodDog === true  ) {
+    para2.innerText = 'is naughty? No!'
+  }
+  else {
+    para2.innerText = 'is naughty? Yes!'
+  }
+
+  const isGoodDog = document.createElement('button', )
 
 }
 dog_1(0)
