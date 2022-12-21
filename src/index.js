b4f7c04d59ed20cbc321c509286003f613fa52b5
Hamza Ahmed <hamza.anwar@outlook.com>
2022-12-21 18:03:34 +0000

    done core

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ const doglist = document.querySelector('ul')
 const dogcard = document.querySelector('.main__dog-section')
 const div1 = document.querySelector('.main__dog-section__desc')
 
+function removeothercards() {
+    dogcard.innerHTML = '<h2> </h2>'
+    
+}
+
 for (let i = 0; i < data.length; i++) {
   const name = data[i].name
   const dognames = document.createElement('li')
   dognames.setAttribute('class', 'dogs-list__button')
   doglist.append(dognames)
   dognames.innerText = name
+  dognames.addEventListener('click', function(){
+    removeothercards()
+    dog_1(i)
+  }) 
 }
 function dog_1(index) {
   
@@ -37,14 +46,16 @@ function dog_1(index) {
   const para2 = document.createElement('p')
   dogcard.append(para2)
   
+  const isGoodDog = document.createElement('button')
+  dogcard.append(isGoodDog)   
+  
   if (data[index].isGoodDog === true  ) {
     para2.innerText = 'is naughty? No!'
+    isGoodDog.innerText = 'good dog'
   }
   else {
     para2.innerText = 'is naughty? Yes!'
+    isGoodDog.innerText = 'bad dog'
   }
 
-  const isGoodDog = document.createElement('button', )
-
 }
-dog_1(0)
