753a7c37f2bd4314b7a00396e513a59da91f7f48
Ahmed Missouri <ahmedmissouri@icloud.com>
2022-12-22 11:03:38 +0000

    Parts 1&2 done along with extension

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ console.log(data);
 const dogListUl = document.querySelector(".dogs-list")
 const dogListButton = document.querySelector(".dogs-list__button")
 const dogMain = document.querySelector(".main")
-const card = document.querySelector(".main__dog-section");
+const card = document.querySelector(".main__dog-section")
+const addDog = document.querySelector(".dogs-list__button--add")
 
 function clearCard() {
-    card.innerHTML = "<h2></h2>";
-  }
+    card.innerHTML = "<h2>No dogs ?!?</h2>"
+}
+
+const clearTopButtons = () => {
+    dogListUl.innerHTML = "<li class='dogs-list__button dogs-list__button--add'>+</li>"
+    const addDog = document.querySelector(".dogs-list__button--add")
+    addDog.addEventListener("click", () => {
+        form()
+    })
+}
 
 const dogList = () => {
+    clearTopButtons()
     for (i = 0; i < data.length; i++) {
     const li = document.createElement("li")
     li.setAttribute("class", "dogs-list__button")
@@ -18,13 +28,13 @@ const dogList = () => {
     li.innerText = data[i].name
     dogListUl.append(li)
     li.addEventListener("click", (eventObj) => {
-        clearCard();
-        selectCard(eventObj.target.id)
-      });
+        clearCard()
+        dogCard(eventObj.target.id)
+      })
     }
 }
 
-const selectCard = (dog) => {
+const dogCard = (dog) => {
     const name = document.querySelector("h2")
     name.innerText = data[dog].name
 
@@ -50,6 +60,7 @@ const selectCard = (dog) => {
 const goodBad = (dog) => {
     const button = document.createElement("button")
     const goodOrBad = document.createElement("p")
+
     if (data[dog].isGoodDog === false) {
         goodOrBad.innerHTML = "<em>Is naughty?</em> yes!"
         button.innerText = "Good dog!"
@@ -69,6 +80,83 @@ const goodBad = (dog) => {
     })
     card.append(goodOrBad)
     card.append(button)
+}
+
+const form = () => {
+    clearCard()
+    const title = document.querySelector("h2")
+    title.innerText = "Add a dog!"
+
+    const form = document.createElement("form")
+    form.setAttribute("class", "form")
+    card.append(form)
+
+    // LABEL FOR NAME
+    const name = document.createElement("label")
+    name.setAttribute("for", "name")
+    name.innerText = "Dog's name"
+    form.append(name)
+    // INPUT FOR NAME
+    const nameInput = document.createElement("input")
+    nameInput.setAttribute("type", "text")
+    nameInput.setAttribute("id", "name")
+    nameInput.setAttribute("name", "name")
+    form.append(nameInput)
+
+    // LABEL FOR IMAGE
+    const image = document.createElement("label")
+    image.setAttribute("for", "image")
+    image.innerText = "Dog's picture"
+    form.append(image)
+    // INPUT FOR IMAGE
+    const imageInput = document.createElement("input")
+    imageInput.setAttribute("type", "url")
+    imageInput.setAttribute("id", "image")
+    imageInput.setAttribute("name", "image")
+    form.append(imageInput)
+
+    // LABEL FOR BIO
+    const bio = document.createElement("label")
+    bio.setAttribute("for", "bio")
+    bio.innerText = "Dog's bio"
+    form.append(bio)
+    // INPUT FOR BIO
+    const bioInput = document.createElement("textarea")
+    bioInput.setAttribute("rows", "5")
+    bioInput.setAttribute("id", "bio")
+    bioInput.setAttribute("name", "bio")
+    form.append(bioInput)
+
+    // SUBMIT
+    const submit = document.createElement("input")
+    submit.setAttribute("type", "submit")
+    submit.setAttribute("id", "submit")
+    submit.setAttribute("name", "submit")
+    submit.setAttribute("value", "Let's add a dog!")
+    submit.setAttribute("class", "form__button")
+    form.append(submit)
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        submitted(nameInput.value, imageInput.value, bioInput.value)
+    })
+}
+
+const submitted = (name, image, bio) => {
+    if (image === "") {
+        image = "https://t4.ftcdn.net/jpg/05/07/58/41/360_F_507584110_KNIfe7d3hUAEpraq10J7MCPmtny8EH7A.jpg"
+    }
+    const newDog = {
+        id: data.length + 1,
+        name: name,
+        image: image,
+        isGoodDog: true,
+        bio: bio
+    }
+    data.unshift(newDog)
+    console.log(newDog)
+    dogList()
+    clearCard()
 }
 
 dogList()
