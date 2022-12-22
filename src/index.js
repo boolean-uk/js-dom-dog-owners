aea1c15e2a936941cf975863e2c0bf509c7d8c20
Manoj kumar <manoj.bhoosani@gmail.com>
2022-12-22 16:59:44 +0100

    first part of the exercise

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 console.log(data);
 
 
-   
-// function listOfDogs(){ : Write this function  after the loop created 
-for (let i = 0; i < data.length; i++) {
-    const main = document.querySelector('.main')
-    const dogsUl = document.querySelector('.dogs-list')
-    const dogNameList = document.createElement('li')
-    //make each li a link +/or add event listener
-    dogNameList.setAttribute('class', 'dogs-list__button')
-    dogNameList.innerText = data[i].name
-    dogsUl.append(dogNameList)
+    for (let i = 0; i < data.length; i++) {
+        const main = document.querySelector('.main')
+        const dogsUl = document.querySelector('.dogs-list')
+        const dogNameList = document.createElement('li')
+        //make each li a link +/or add event listener
+        dogNameList.setAttribute('class', 'dogs-list__button')
+        dogNameList.setAttribute('id', i )
+        dogNameList.innerText = data[i].name
+        dogsUl.append(dogNameList)
     
-}
-
-function createDogCard(dogData) {
-    for (i = 0; i < data.length; i++) {
+        dogNameList.addEventListener('click', function (eventObj) {
+            console.log(eventObj.target.id)
+            const mainSection = document.createElement('section')
+            mainSection.setAttribute('class', 'main__dog-section')
+            
+            const dogCard = createDogCard(eventObj.target.id)
+            
+        });  
+    }
+        
+    function createDogCard(dogData) {
         //select area
-        const dogCards = document.querySelector('.main__dog-section')
-        const dogH2 = document.querySelector('.main__dog-section > h2')
-        dogH2.innerText = data[i].name
+        const dogCards = document.querySelector('section')
+        console.log(dogCards)
+        dogCards.innerText =''
+        const dogH2 = document.createElement('h2')
+        dogH2.innerText = data[dogData].name
+        dogCards.append(dogH2)
         console.log(dogH2)
 
 
@@ -28,56 +37,39 @@ function createDogCard(dogData) {
         const dogImage = document.createElement('img')
         dogImage.setAttribute('type', 'url')
         const dogProfilePic = new Image(256)
-        dogProfilePic.src = data[i].image;
+        dogProfilePic.src = data[dogData].image;
         dogImg.append(dogProfilePic)
+
+        // creating the div for whole bio section
+        const bioInfo = document.createElement('div')
+        bioInfo.setAttribute('class', 'main__dog-section__desc')
+        dogCards.append(bioInfo)
+
 
         //Adding the Bio
         const dogBioSection = document.querySelector('.main__dog-section__desc')
 
         const dogBio = document.createElement('h3')
         dogBio.innerText = 'Bio'
-        dogImg.append(dogBio)
+        bioInfo.append(dogBio)
 
         //Adding bioInfo
         const dogBioInfo = document.createElement('p')
-        dogBioInfo.innerText = data[i].bio
-        dogBio.append(dogBioInfo)
+        dogBioInfo.innerText = data[dogData].bio
+        bioInfo.append(dogBioInfo)
 
         //Is a good dog? 
         // create the function for the true or false
         const goodDog = document.createElement('p')
-        goodDog.innerText = `is naugthy? ${data[i].isGoodDog}`
-        dogBio.append(goodDog)
+        goodDog.innerText = `is naugthy? ${data[dogData].isGoodDog}`
+        dogCards.append(goodDog)
 
 
         // try to make some space
         const newButton = document.createElement('button')
         newButton.innerText = `Good Dog`
-        dogBio.append(newButton)
-        //
-
-        break
+        dogCards.append(newButton)
+       
     }
-}
 
-// createDogCard(dogData)
-
-
-
-    // reset main section on click
-        dogNameList.addEventListener('click', function () {
-        main.innerHTML = ''
-        const mainSection = document.createElement('section')
-        mainSection.setAttribute('class', 'main__dog-section')
-        const dogData = data[i]
-        const dogCard = createDogCard(dogData)
-        main.append(dogCard)
-        console.log(dogNameList)
-    });
-
-
-
-
-
-// second part add the dog names and card to the each every dog names,
-// than add the all information about the dog
+// create the form 
