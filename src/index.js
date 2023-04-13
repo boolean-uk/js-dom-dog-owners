4b65992b57808da7d9baa14c480fb8d57ae0b738
Ian Grantham <dev.iangrantham@gmail.com>
2023-04-13 15:20:40 +0100

    main card section started

--- src/index.js
+++ src/index.js
@@ -12,15 +12,48 @@ console.log(data);
     // append the li to the ul
     
 for (i = 0; i < data.length; i++) {
-    const dogInfo = data[i];
-    const ul = document.querySelector('ul')
-    const li = document.createElement('li');
-    li.innerText = dogInfo.name;
-    li.setAttribute("class", "dogs-list__button");
-    ul.append(li);
+    // making the dog list buttons
+        const dogInfo = data[i];
+        const ul = document.querySelector('ul')
+        const li = document.createElement('li');
+        li.innerText = dogInfo.name;
+        li.setAttribute("class", "dogs-list__button");
+        ul.append(li);
+
+    // create the main card
+        // find the section
+        const section = document.querySelector('.main__dog-section')
+        // find the h2
+        const h2 = document.querySelector('h2')
+        // change the inner text to the dog's name
+        const dogName = dogInfo.name
+        h2.innerText = dogName
 }
 
 
-// create the main card 
+
+// render the name 
+    // create h2 for each dog
+    // append the created h2 into the section
+
+
+
+
+// clear the data in the card
+
 // inputing the info to the card
 // generate a click event that displays the card with all the info for the dog that is being clicked
+
+
+
+
+
+
+
+
+
+
+// ---saved for later ---
+    // loop through the data
+    // seperate out the above loop into a function
+    // call that for the buttons AND cards
