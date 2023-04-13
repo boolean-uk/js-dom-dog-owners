c5c7c099a671077d048595fb299055340ffe8a08
Priya <ps975076@gmail.com>
2023-04-13 14:49:58 +0100

    buttons done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
+
+const main = document.querySelector('.main')
+
+const dogsList = document.querySelector('.dogs-list')
+
+const mainDogSection = document.querySelector('.main__dog-section')
+
+// Main
+
+function createButton() {
+    for (let i = 0; i < data.length; i++) {
+        const dogLi = document.createElement('li');
+        dogLi.setAttribute('class','dogs-list__button');
+        // dogLi.setAttribute('id', `menu-${i}`);
+        dogLi.innerText = data[i].name
+        dogsList.append(dogLi);
+    }
+}
+
+// Dogs List
+
+
+
+// Main-Dog-Section
+
+// create a constant for the "li"
+// set attributes of the "li"
+// set the innerText of the "li"
+// append in to the "ul" constant
+
+createButton()
