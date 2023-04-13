9be0ed8b3231b307e0c54e49adfb084b82b166b6
Alex Shaw <alexjshaw@gmail.com>
2023-04-13 15:21:09 +0100

    dog card function created, partially completed

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ console.log(data);
 // WRITE YOUR CODE BELOW!
 
 const main = document.querySelector('.main')
-
 const dogsList = document.querySelector('.dogs-list')
-
 const mainDogSection = document.querySelector('.main__dog-section')
 
 // Main
@@ -14,10 +12,38 @@ function createButton() {
     for (let i = 0; i < data.length; i++) {
         const dogLi = document.createElement('li');
         dogLi.setAttribute('class','dogs-list__button');
-        // dogLi.setAttribute('id', `menu-${i}`);
+        dogLi.setAttribute('id', i);
         dogLi.innerText = data[i].name
         dogsList.append(dogLi);
+
+        // dogid
     }
+}
+
+// Dog Cards
+
+function createCard(dogID) {
+
+    main.innerHTML = ``
+
+    const currentDog = data[dogID]
+    const dogSection = document.createElement(`section`)
+    dogSection.setAttribute(`class`, `main__dog-section`)
+
+    const dogName = document.createElement(`h2`)
+    dogName.innerText = currentDog.name
+    dogSection.append(dogName)
+
+    const dogImage = document.createElement(`img`)
+    dogImage.setAttribute(`src`, currentDog.image)
+    dogImage.setAttribute(`alt`, ``)
+    dogSection.append(dogImage)
+
+    
+
+    main.append(dogSection)
+
+
 }
 
 // Dogs List
@@ -32,3 +58,4 @@ function createButton() {
 // append in to the "ul" constant
 
 createButton()
+createCard(0)
