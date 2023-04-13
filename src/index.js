827f789db634469d323e660e90b4fb42722e14ec
Ian Grantham <dev.iangrantham@gmail.com>
2023-04-13 16:49:30 +0100

    main card content created

--- src/index.js
+++ src/index.js
@@ -12,35 +12,61 @@ console.log(data);
     // append the li to the ul
     
 for (i = 0; i < data.length; i++) {
+    
     // making the dog list buttons
         const dogInfo = data[i];
         const ul = document.querySelector('ul')
         const li = document.createElement('li');
         li.innerText = dogInfo.name;
         li.setAttribute("class", "dogs-list__button");
-    ul.append(li);
-    const id = dogInfo.id;
-    li.setAttribute("id", id)
+        ul.append(li);
+        const id = dogInfo.id;
+        li.setAttribute("id", id)
 
-    // create the main card
+   
+// create the main card
         // find the section
         const section = document.querySelector('.main__dog-section')
+        
         // find the h2
         const h2 = document.querySelector('h2')
         // change the inner text to the dog's name
         const dogName = dogInfo.name
         h2.innerText = dogName
+       
+
+
+// add in dog img
+        // create img container
+       
+        const img = document.createElement('img')
+        // have the path to img from top data
+        
+        img.src = dogInfo.image
+        // append to the card
+        h2.append(img)
+
+// make div
+        const div = document.createElement('div')
+        div.setAttribute('class', 'main__dog-section__desc')
+        h2.append(div)
+
+    // make h3
+            const h3 = document.createElement('h3')
+            h3.innerText = 'Bio'
+            div.append(h3)
+    // make p
+            const p = document.createElement('p')
+            p.innerText = dogInfo.bio
+            div.append(p)
+        
+
 
 
 
 // render the name 
     // create h2 for each dog
     // append the created h2 into the section
-
-
-
-
-// clear the data in the card
 
     
 //    this function should display the correct dog
@@ -49,12 +75,27 @@ for (i = 0; i < data.length; i++) {
     // find the correct dog
     // display new content using the same card
     
+  
+    
+    // dogSelectButton.addEventListener('click', function() {
+    //     renderDog(i)
+    //   })
+
     li.addEventListener('click', function() {
         console.log('How dare you click me!?')
         h2.innerText = "Did you click on me????"
+
+        // read the id of which button has been clicked
+        // const clicky = document.getElementById(elementId, '')
+
+
+
+        // use that to find dog's info
+        // populate card with associated dog's info
     })
+  
     
-       
+    
     
 
 // inputing the info to the card
