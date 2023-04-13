a23e8888b1589d6fbf1b3cb6e782286eb551205d
carolarruda <carolinacalouroarruda@gmail.com>
2023-04-13 18:24:43 +0200

    working on click event

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ console.log(data);
 for (i = 0; i < data.length; i++) {
     
     // making the dog list buttons
+ 
         const dogInfo = data[i];
         const ul = document.querySelector('ul')
         const li = document.createElement('li');
@@ -22,54 +23,57 @@ for (i = 0; i < data.length; i++) {
         ul.append(li);
         const id = dogInfo.id;
         li.setAttribute("id", id)
+    
 
+
+    // function dogFinder() {
    
-// create the main card
-        // find the section
-        const section = document.querySelector('.main__dog-section')
+    // create the main card
+    // find the section
+    const section = document.querySelector('.main__dog-section')
         
-        // find the h2
-        const h2 = document.querySelector('h2')
-        // change the inner text to the dog's name
-        const dogName = dogInfo.name
-        h2.innerText = dogName
+    // find the h2
+    const h2 = document.querySelector('h2')
+    // change the inner text to the dog's name
+    const dogName = dogInfo.name
+    h2.innerText = dogName
        
 
 
-// add in dog img
-        // create img container
+    // add in dog img
+    // create img container
        
-        const img = document.createElement('img')
-        // have the path to img from top data
+    const img = document.createElement('img')
+    // have the path to img from top data
         
-        img.src = dogInfo.image
-        // append to the card
-        h2.append(img)
+    img.src = dogInfo.image
+    // append to the card
+    h2.append(img)
 
-// make div
-        const div = document.createElement('div')
-        div.setAttribute('class', 'main__dog-section__desc')
-        h2.append(div)
+    // make div
+    const div = document.createElement('div')
+    div.setAttribute('class', 'main__dog-section__desc')
+    h2.append(div)
 
     // make h3
-            const h3 = document.createElement('h3')
-            h3.innerText = 'Bio'
-            div.append(h3)
+    const h3 = document.createElement('h3')
+    h3.innerText = 'Bio'
+    div.append(h3)
     // make p
-            const p = document.createElement('p')
-            p.innerText = dogInfo.bio
-            div.append(p)
+    const p = document.createElement('p')
+    p.innerText = dogInfo.bio
+    div.append(p)
         
 
 
 
 
-// render the name 
+    // render the name 
     // create h2 for each dog
     // append the created h2 into the section
 
     
-//    this function should display the correct dog
+    //    this function should display the correct dog
     // clear out the card information displayed - refresh the main content
     // match the header list
     // find the correct dog
@@ -81,21 +85,35 @@ for (i = 0; i < data.length; i++) {
     //     renderDog(i)
     //   })
 
-    li.addEventListener('click', function() {
+    li.addEventListener('click', function (event) {
         console.log('How dare you click me!?')
-        h2.innerText = "Did you click on me????"
+        // h2.innerText = "Did you click on me????"
+        console.log('this is event', event);
 
-        // read the id of which button has been clicked
-        // const clicky = document.getElementById(elementId, '')
+        console.log('target', event.target);
+        
+        console.log('value', event.target.id);
+        test(event.target.id)
 
-
-
-        // use that to find dog's info
-        // populate card with associated dog's info
     })
-  
+
+
     
-    
+    // }
+
+}
+
+const test = (value) => {
+    console.log('this is a test', value);
+}
+
+    // read the id of which button has been clicked
+   
+
+
+
+    // use that to find dog's info
+    // populate card with associated dog's info
     
 
 // inputing the info to the card
@@ -103,7 +121,6 @@ for (i = 0; i < data.length; i++) {
 
 
 
-}
 
 
 
