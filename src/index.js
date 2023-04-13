5cc0cd912103e16c87f1b9c64a9549c47dbb9200
Mantas Navickis <navickis@gmail.com>
2023-04-14 00:00:47 +0200

    work in progress

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ console.log(data)
 
 const unorderedList = document.querySelector('.dogs-list')
 
-
 for (let i = 0; i < data.length; i++) {
   const listItem = document.createElement('li')
   listItem.setAttribute('class', 'dogs-list__button')
@@ -16,59 +15,60 @@ for (let i = 0; i < data.length; i++) {
   const dogName = data[i].name
   listItem.innerText = dogName
 
+  
+  const dogCard = document.querySelector('section')
+  function removeCard() {
+    dogCard.remove()
+  }
+  listItem.addEventListener('click', () => {
+    removeCard()
+    dogCards()
+
+  })
   function dogCards() {
-    //Create a section
+    // Create a section
     const main = document.querySelector('.main')
-    const section = document.createElement("section")
-    section.setAttribute("class", "main__dog-section")
+    const section = document.createElement('section')
+    section.setAttribute('class', 'main__dog-section')
     main.append(section)
-    //create H2
+    // create H2
     const cardName = document.createElement('h2')
     cardName.innerText = data[i].name
     section.append(cardName)
-    //create IMG
+    // create IMG
     const dogImg = document.createElement('img')
-    dogImg.setAttribute("src", data[i].image)
+    dogImg.setAttribute('src', data[i].image)
     section.append(dogImg)
-    //Create a Div
+    // Create a Div
     const bioDiv = document.createElement('div')
-    bioDiv.setAttribute('class',"main__dog-section__desc")
+    bioDiv.setAttribute('class', 'main__dog-section__desc')
     section.append(bioDiv)
-    //create h3
+    // create h3
     const bioH3 = document.createElement('h3')
     bioH3.innerText = 'Bio'
     bioDiv.append(bioH3)
-    //create p
+    // create p
     const bioP = document.createElement('p')
     bioP.innerText = data[i].bio
     bioDiv.append(bioP)
-    //Naughty Dog Text
+    // Naughty Dog Text
     const pTag = document.createElement('p')
     const emTag = document.createElement('em')
     const button = document.createElement('button')
-    pTag.innerText = "Is naughty?"
-    
+    pTag.innerText = 'Is naughty?'
+
     if (data[i].isGoodDog === true) {
-        emTag.innerText = " Yes!"
-        button.innerText = "Good dog!"
-        
+      emTag.innerText = ' Yes!'
+      button.innerText = 'Good dog!'
     } else if (data[i].isGoodDog === false) {
-        emTag.innerText = " No!"
-        button.innerText = "Bad Dog!"
+      emTag.innerText = ' No!'
+      button.innerText = 'Bad Dog!'
     }
-    
+
     pTag.append(emTag)
     section.append(pTag)
     section.append(button)
-   
-    
-    
-
-}
-dogCards()
+  }
+  // dogCards()
 }
 // dog Card
-
-
-
-
