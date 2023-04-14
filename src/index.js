8d97fb88141492019a75bb42c66ebd28d5f03ce4
Ian Grantham <dev.iangrantham@gmail.com>
2023-04-14 13:35:33 +0100

    new dog form added

--- src/index.js
+++ src/index.js
@@ -69,6 +69,10 @@ for (i = 0; i < data.length; i++) {
     })
 }
 
+// 1 - When the plus button is clicked, it should replace the main card 
+// with a form to add a new dog to the list. You'll find a template for 
+// the form on the HTML page. 
+
 // --- CLICKING PLUS ---
 
 // find plus button
@@ -92,86 +96,64 @@ for (i = 0; i < data.length; i++) {
     // append h2
         section.append(h2);
     
-
-
-
-
+// make new form
+    //form
+        const form = document.createElement('form')
+        form.setAttribute('class', 'form')
+        section.append(form)
+        // label - dog's name
+            const newDogName = document.createElement('label')
+            newDogName.setAttribute('for', 'name')
+            newDogName.innerText = "Dog's name"
+            form.append(newDogName)
+        //input id=name
+            const inputName = document.createElement('input')
+            inputName.setAttribute('type', 'text')
+            inputName.setAttribute('id', 'name')
+            inputName.setAttribute('name', 'name')
+            form.append(inputName)
+        //label for=image
+            const newDogImgLabel = document.createElement('label')
+            newDogImgLabel.setAttribute('for', 'image')
+            newDogImgLabel.innerText = "Dog's picture"
+            form.append(newDogImgLabel)
+        // input image
+            // input 
+            const newDogImgInput = document.createElement('input')
+            newDogImgInput.setAttribute('type', 'url')
+            newDogImgInput.setAttribute('id', 'image')
+            newDogImgInput.setAttribute('name', 'image')
+            form.append(newDogImgInput)
+        //label for=bio
+            const newDogBio = document.createElement('label')
+            newDogBio.setAttribute('for', 'bio')
+            newDogBio.innerText = "Dog's Bio"
+            form.append(newDogBio)
+        //textarea
+            const newDogBioTextArea = document.createElement('textarea')
+            newDogBioTextArea.setAttribute('rows', '5')
+            newDogBioTextArea.setAttribute('id', 'bio')
+            newDogBioTextArea.setAttribute('name', 'bio')
+            form.append(newDogBioTextArea)
+        //input - submit
+            const newDogSubmit = document.createElement('submit')
+            newDogSubmit.setAttribute('type','submit')
+            newDogSubmit.setAttribute('id','submit')
+            newDogSubmit.setAttribute('name','submit')
+            newDogSubmit.setAttribute('value',"Let's add a dog!")
+            newDogSubmit.setAttribute('class',"form__button")
+            newDogSubmit.innerText = "Let's add a dog!"
+            form.append(newDogSubmit)
     })
 
 
 
 
 /*
-1 - When the plus button is clicked, it should replace the main card 
-with a form to add a new dog to the list. You'll find a template for 
-the form on the HTML page. 
 
 2 - Once the form is submitted, add the new dog to the beginning of 
 the list, right next to the plus button.
 
 
-
-
-1 - 
-
-
-make new form
-    section
-        h2
-        form
-            label for=name
-            input id=name
-            label for=image
-            input id=image
-            label
-            textarea
-            input - submit
-
-
-
-
-    This is a template for the add dog form
-
-    <section class="main__dog-section">
-
-      <h2>Add a new Dog</h2>
-
-      <form class="form">
-
-        <label for="name">
-            Dog's name
-        </label>
-
-        <input 
-            type="text" 
-            id="name" 
-            name="name">
-
-        <label for="image">
-            Dog's picture
-        </label>
-
-        <input 
-            type="url"
-            id="image"
-            name="image">
-
-        <label for="bio">
-            Dog's bio
-        </label>
-
-        <textarea 
-            rows="5" 
-            id="bio" 
-            name="bio">
-        </textarea>
-
-        <input type="submit" 
-            id="submit" name="submit" 
-            value="Let's add a dog!" 
-            class="form__button">
-      </form>
-
-  </section>
 
 */
