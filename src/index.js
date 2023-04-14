6d8ee3ac7721c995612490dd7466f28a64241e5a
Ian Grantham <dev.iangrantham@gmail.com>
2023-04-14 11:55:20 +0100

    plus button click event added

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ for (i = 0; i < data.length; i++) {
         li.setAttribute("id", id)
         
 // listen for click
-        li.addEventListener('click', function (event) {
+        li.addEventListener('click', function () {
 
 // --- MAIN CARD ---
 
@@ -68,3 +68,110 @@ for (i = 0; i < data.length; i++) {
                 div.append(p)
     })
 }
+
+// --- CLICKING PLUS ---
+
+// find plus button
+    const plusButton = document.querySelector('.dogs-list__button--add')
+// click event for + button    
+    plusButton.addEventListener('click', function() {
+        console.log('hello')
+// when clicked
+    // find main
+        const main = document.querySelector('main');
+    // clear section
+        main.innerHTML = "";
+    // make new section
+        const section = document.createElement('section');
+        section.setAttribute("class", "main__dog-section")
+        main.append(section)
+    //make the h2
+        const h2 = document.createElement('h2')        
+    // change the h2 inner text to Add a new Dog
+        h2.innerText  = "Add a new dog"
+    // append h2
+        section.append(h2);
+    
+
+
+
+
+    })
+
+
+
+
+/*
+1 - When the plus button is clicked, it should replace the main card 
+with a form to add a new dog to the list. You'll find a template for 
+the form on the HTML page. 
+
+2 - Once the form is submitted, add the new dog to the beginning of 
+the list, right next to the plus button.
+
+
+
+
+1 - 
+
+
+make new form
+    section
+        h2
+        form
+            label for=name
+            input id=name
+            label for=image
+            input id=image
+            label
+            textarea
+            input - submit
+
+
+
+
+    This is a template for the add dog form
+
+    <section class="main__dog-section">
+
+      <h2>Add a new Dog</h2>
+
+      <form class="form">
+
+        <label for="name">
+            Dog's name
+        </label>
+
+        <input 
+            type="text" 
+            id="name" 
+            name="name">
+
+        <label for="image">
+            Dog's picture
+        </label>
+
+        <input 
+            type="url"
+            id="image"
+            name="image">
+
+        <label for="bio">
+            Dog's bio
+        </label>
+
+        <textarea 
+            rows="5" 
+            id="bio" 
+            name="bio">
+        </textarea>
+
+        <input type="submit" 
+            id="submit" name="submit" 
+            value="Let's add a dog!" 
+            class="form__button">
+      </form>
+
+  </section>
+
+*/
