e035284b89df28d74e4979ac874f492e47ea18c4
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-04-16 15:59:01 +0200

    Exercise finished

--- src/index.js
+++ src/index.js
@@ -11,104 +11,116 @@ const main = document.querySelector('.main')
 const formsSection = document.querySelector('.main__dog-section')
 const plusButton = document.querySelector('.dogs-list__button--add')
 
+
+
 // Loop to access the elements inside of the data.js file
-for (let i = 0; i < data.length; i++) {
-  const listItem = document.createElement('li')
-  listItem.setAttribute('class', 'dogs-list__button')
-  unorderedList.append(listItem)
-  const dogName = data[i].name
-  listItem.innerText = dogName
 
-  function removeCard() {
-    main.innerHTML = ''
-  }
-  listItem.addEventListener('click', () => {
-    // removeCard()
-    dogCards()
-  })
-
-  function dogCards() {
-    // Create a section
-    // const main = document.querySelector('.main')
-    // const section = document.querySelector('section')
-    // section.setAttribute('class', 'main__dog-section')
-    // main.append(section)
-    // create H2
-    formsSection.innerHTML = ''
-    const cardName = document.createElement('h2')
-    cardName.innerText = data[i].name
-    formsSection.append(cardName)
-    // create IMG
-    const dogImg = document.createElement('img')
-    dogImg.setAttribute('src', data[i].image)
-    formsSection.append(dogImg)
-    // Create a Div
-    const bioDiv = document.createElement('div')
-    bioDiv.setAttribute('class', 'main__dog-section__desc')
-    formsSection.append(bioDiv)
-    // create h3
-    const bioH3 = document.createElement('h3')
-    bioH3.innerText = 'Bio'
-    bioDiv.append(bioH3)
-    // create p
-    const bioP = document.createElement('p')
-    bioP.innerText = data[i].bio
-    bioDiv.append(bioP)
-    // Naughty Dog Text
-    const pTag = document.createElement('p')
-    const emTag = document.createElement('em')
-    const button = document.createElement('button')
-    pTag.innerText = 'Is naughty?'
-
-    function changeButton() {
+function renderList(){
+  for (let i = 0; i < data.length; i++) {
+    
+    const listItem = document.createElement('li')
+    listItem.setAttribute('class', 'dogs-list__button')
+    unorderedList.append(listItem)
+    const dogName = data[i].name
+    listItem.innerText = dogName
+  
+    listItem.addEventListener('click', () => {
+     
+      dogCards()
+    })
+  
+    function dogCards() {
+      // Create a section
+      // const main = document.querySelector('.main')
+      // const section = document.querySelector('section')
+      // section.setAttribute('class', 'main__dog-section')
+      // main.append(section)
+      // create H2
+      formsSection.innerHTML = ''
+      const cardName = document.createElement('h2')
+      cardName.innerText = data[i].name
+      formsSection.append(cardName)
+      // create IMG
+      const dogImg = document.createElement('img')
+      dogImg.setAttribute('src', data[i].image)
+      formsSection.append(dogImg)
+      // Create a Div
+      const bioDiv = document.createElement('div')
+      bioDiv.setAttribute('class', 'main__dog-section__desc')
+      formsSection.append(bioDiv)
+      // create h3
+      const bioH3 = document.createElement('h3')
+      bioH3.innerText = 'Bio'
+      bioDiv.append(bioH3)
+      // create p
+      const bioP = document.createElement('p')
+      bioP.innerText = data[i].bio
+      bioDiv.append(bioP)
+      // Naughty Dog Text
+      const pTag = document.createElement('p')
+      const emTag = document.createElement('em')
+      const button = document.createElement('button')
+      pTag.innerText = 'Is naughty?'
+  
+      function changeButton() {
+        if (data[i].isGoodDog === true) {
+          emTag.innerText = ' No!'
+          button.innerText = 'Bad dog!'
+          data[i].isGoodDog = false
+          
+        } else if (data[i].isGoodDog === false) {
+          emTag.innerText = ' Yes!'
+          button.innerText = 'Good dog!'
+          data[i].isGoodDog = true
+        }
+      }
+  
+      button.addEventListener('click', changeButton)
+  
       if (data[i].isGoodDog === true) {
-        emTag.innerText = ' No!'
-        button.innerText = 'Bad dog!'
-        data[i].isGoodDog = false
-        
-      } else if (data[i].isGoodDog === false) {
         emTag.innerText = ' Yes!'
         button.innerText = 'Good dog!'
-        data[i].isGoodDog = true
+      } else if (data[i].isGoodDog === false) {
+        emTag.innerText = ' No!'
+        button.innerText = 'Bad dog!'
       }
+  
+      pTag.append(emTag)
+      formsSection.append(pTag)
+      formsSection.append(button)
     }
-
-    button.addEventListener('click', changeButton)
-
-    if (data[i].isGoodDog === true) {
-      emTag.innerText = ' Yes!'
-      button.innerText = 'Good dog!'
-    } else if (data[i].isGoodDog === false) {
-      emTag.innerText = ' No!'
-      button.innerText = 'Bad dog!'
-    }
-
-    pTag.append(emTag)
-    formsSection.append(pTag)
-    formsSection.append(button)
+  
+    //   <!--
+    //   This is a template for the add dog form
+  
+    //   <section class="main__dog-section">
+    //     <h2>Add a new Dog</h2>
+    //     <form class="form">
+  
+    //       <label for="name">Dog's name</label>
+    //       <input type="text" id="name" name="name">
+  
+    //       <label for="image">Dog's picture</label>
+    //       <input type="url" id="image" name="image">
+  
+    //       <label for="bio">Dog's bio</label>
+    //       <textarea rows="5" id="bio" name="bio"></textarea>
+  
+    //       <input type="submit" id="submit" name="submit" value="Let's add a dog!" class="form__button">
+    //     </form>
+    // </section>
+    //  -->
   }
 
-  //   <!--
-  //   This is a template for the add dog form
-
-  //   <section class="main__dog-section">
-  //     <h2>Add a new Dog</h2>
-  //     <form class="form">
-
-  //       <label for="name">Dog's name</label>
-  //       <input type="text" id="name" name="name">
-
-  //       <label for="image">Dog's picture</label>
-  //       <input type="url" id="image" name="image">
-
-  //       <label for="bio">Dog's bio</label>
-  //       <textarea rows="5" id="bio" name="bio"></textarea>
-
-  //       <input type="submit" id="submit" name="submit" value="Let's add a dog!" class="form__button">
-  //     </form>
-  // </section>
-  //  -->
 }
+
+renderList()
+
+function addNewDog(){
+  
+}
+
+
 plusButton.addEventListener('click', () => {
   // const formslist = document.querySelector('.dogs-list__button--add')
   formsSection.innerHTML = ''
@@ -160,9 +172,57 @@ plusButton.addEventListener('click', () => {
   formSubmitButton.setAttribute('value', "Let's add a dog!")
   formSubmitButton.setAttribute('class', 'form__button')
   form.append(formSubmitButton)
-})
 
-// plusButton.addEventListener('click', () => {
-//   forms()
+  
+  // formSubmitButton.addEventListener('submit', () => {
+    //   //event.defaultPrevented();
+    
+    //   //newDogObj.addEventListener("click", () =>{
+  //       newDogObj ={
+    //           id: data.length + 1,
+    //           name: formInputName.value,
+    //           image: formInputImage.value,
+    //           isGoodDog: true,
+    //           bio: formInputBio.value
+    //       }
+    //       //cards(i)
+    //       data.unshift(newDogObj)
+    // })
 
-// })
+    function addNewDog(){
+      data.unshift({
+        id: data.length + 1,
+        name: formInputName.value,
+        image: formInputImage.value,
+        isGoodDog: true,
+        bio: formInputBio.value
+      })
+    }
+
+
+
+    formSubmitButton.addEventListener('click', (event) => {
+      
+      // listItem.remove()
+      for (let i = 0; i <= data.length; i++){
+        document.querySelector('.dogs-list__button').remove()
+      }
+      unorderedList.append(plusButton)
+      addNewDog()
+      renderList()
+      formInputName.value = ''
+      formInputImage.value = ''
+      formInputBio.value = ''
+      event.preventDefault()
+      
+      
+
+      
+        
+    })
+
+
+  })
+  
+
+  
