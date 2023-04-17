4b78db08fb4affe41db15292b70ef15a984de4bc
Timmyctt <timmyc1990@yahoo.com>
2023-04-17 18:43:29 +0100

    updated for adding new dog

--- src/index.js
+++ src/index.js
@@ -1,89 +1,93 @@
 // WRITE YOUR CODE BELOW!
 console.log(data);
 // 1. Create more listed item in the loop.
-for (let i = 0; i < data.length; i++) {
-  const dogsList = document.querySelector(".dogs-list");
-  const li = document.createElement("li");
-  li.innerText = data[i].name;
+function init() {
+  for (let i = 0; i < data.length; i++) {
+    const dogsList = document.querySelector(".dogs-list");
+    const li = document.createElement("li");
+    li.innerText = data[i].name;
 
-  // 2. Add dog names into listed items.
-  li.setAttribute("class", "dogs-list__button", "dogs-list__button--add");
-  dogsList.append(li);
-  // 3. Add eventListener for click.
-  li.addEventListener("click", function () {
-    // 4. Add empty .innerHTML for removing the section.
-    const main = document.querySelector("main");
-    main.innerText = "";
-    // 5. Add dog's sections.
-    const section = document.createElement("section");
-    section.setAttribute("class", "main__dog-section");
-    main.append(section);
+    // 2. Add dog names into listed items.
+    li.setAttribute("class", "dogs-list__button", "dogs-list__button--add");
+    dogsList.append(li);
+    // 3. Add eventListener for click.
+    li.addEventListener("click", function () {
+      // 4. Add empty .innerHTML for removing the section.
+      const main = document.querySelector("main");
+      main.innerText = "";
+      // 5. Add dog's sections.
+      const section = document.createElement("section");
+      section.setAttribute("class", "main__dog-section");
+      main.append(section);
 
-    const h2 = document.createElement("h2");
-    section.append(h2);
-    h2.innerText = data[i].name;
+      const h2 = document.createElement("h2");
+      section.append(h2);
+      h2.innerText = data[i].name;
 
-    const img = document.createElement("img");
-    section.append(img);
-    img.src = data[i].image;
-    img.setAttribute("width", "400");
-    img.setAttribute("height", "300");
+      const img = document.createElement("img");
+      section.append(img);
+      img.src = data[i].image;
+      img.setAttribute("width", "400");
+      img.setAttribute("height", "300");
 
-    const div = document.createElement("div");
-    section.append(div);
-    div.setAttribute("class", "main__dog-section__desc");
+      const div = document.createElement("div");
+      section.append(div);
+      div.setAttribute("class", "main__dog-section__desc");
 
-    const h3 = document.createElement("h3");
-    div.append(h3);
-    h3.innerText = "Bio";
+      const h3 = document.createElement("h3");
+      div.append(h3);
+      h3.innerText = "Bio";
 
-    const p = document.createElement("p");
-    div.append(p);
-    p.innerText = data[i].bio;
+      const p = document.createElement("p");
+      div.append(p);
+      p.innerText = data[i].bio;
 
-    const pTwo = document.createElement("p");
-    section.append(pTwo);
+      const pTwo = document.createElement("p");
+      section.append(pTwo);
 
-    let naughty;
-    if (data[i].isGoodDog === true) {
-      naughty = " No!";
-    } else {
-      naughty = " Yes!";
-    }
-    pTwo.innerText = naughty;
-
-    const em = document.createElement("em");
-    pTwo.insertAdjacentElement("afterbegin", em);
-    em.innerText = "Is naughty?  ";
-
-    const button = document.createElement("button");
-    section.append(button);
-
-    if (data[i].isGoodDog === true) {
-      button.innerText = "Bad Dog";
-    } else {
-      button.innerText = "Good Dog";
-    }
-
-    button.addEventListener("click", function () {
+      let naughty;
       if (data[i].isGoodDog === true) {
-        button.innerText = "Good Dog";
-        naughty = " Yes!";
-        pTwo.innerText = naughty;
-        const em = document.createElement("em");
-        pTwo.insertAdjacentElement("afterbegin", em);
-        em.innerText = "Is naughty?  ";
-      } else {
-        button.innerText = "Bad Dog";
         naughty = " No!";
-        pTwo.innerText = naughty;
-        const em = document.createElement("em");
-        pTwo.insertAdjacentElement("afterbegin", em);
-        em.innerText = "Is naughty?  ";
+      } else {
+        naughty = " Yes!";
       }
+      pTwo.innerText = naughty;
+
+      const em = document.createElement("em");
+      pTwo.insertAdjacentElement("afterbegin", em);
+      em.innerText = "Is naughty?  ";
+
+      const button = document.createElement("button");
+      section.append(button);
+
+      if (data[i].isGoodDog === true) {
+        button.innerText = "Bad Dog";
+      } else {
+        button.innerText = "Good Dog";
+      }
+
+      button.addEventListener("click", function () {
+        if (data[i].isGoodDog === true) {
+          button.innerText = "Good Dog";
+          naughty = " Yes!";
+          pTwo.innerText = naughty;
+          const em = document.createElement("em");
+          pTwo.insertAdjacentElement("afterbegin", em);
+          em.innerText = "Is naughty?  ";
+        } else {
+          button.innerText = "Bad Dog";
+          naughty = " No!";
+          pTwo.innerText = naughty;
+          const em = document.createElement("em");
+          pTwo.insertAdjacentElement("afterbegin", em);
+          em.innerText = "Is naughty?  ";
+        }
+      });
     });
-  });
+  }
 }
+
+init();
 
 // select "+" button
 const plus = document.querySelector(".dogs-list__button");
@@ -146,12 +150,25 @@ plus.addEventListener("click", function () {
   input3.setAttribute("class", "form__button");
   form.append(input3);
   // add eventlistener to submit button
-  input3.addEventListener("submit", function (event) {
+  form.addEventListener("submit", function (event) {
     event.preventDefault();
+    const dogsList = document.querySelector(".dogs-list");
+    dogsList.innerText = "";
+    const plusbutton = document.createElement("li");
+    plusbutton.innerText = "+";
+    plusbutton.setAttribute(
+      "class",
+      "dogs-list__button",
+      "dogs-list__button--add"
+    );
+    dogsList.appendChild(plusbutton);
     const addnewdogList = document.querySelector(".dogs-list");
     const newli = document.createElement("li");
-    li.innerText = "New Dog test";
+    const newdog = document.getElementById("name").value;
+    newli.innerText = newdog;
+    newli.setAttribute("class", "dogs-list__button", "dogs-list__button--add");
     addnewdogList.appendChild(newli);
+    init();
   });
 });
 
