7491e439c6e2edda0c1e0a1f76529a224dc50a00
irlydo <fokum8@gmail.com>
2023-10-11 20:30:23 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,164 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
+const dogButtons = document.querySelector("ul");
+
+for (let i = 0; i < data.length; i++) {
+
+    // Dog selector buttons
+
+    const dogs = data[i];
+
+    const dogList = document.createElement("li");
+    dogList.innerText = dogs.name
+    dogButtons.append(dogList)
+
+    dogList.setAttribute('class', 'dogs-list__button');
+}
+
+    //  Dog card
+
+const allDogs = document.querySelectorAll('.dogs-list__button')
+
+const bio = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum,minima voluptates libero cumque rerum consequatur optio aliquid sinteum maxime illo laborum omnis quo ab rem cupiditate nullaperspiciatis ipsum!"
+
+const firstDogButton = allDogs[1]
+const secondDogButton = allDogs[2]
+const thirdDogButton = allDogs[3]
+const fourthDogButton = allDogs[4]
+const fifthDogButton = allDogs[5]
+const sixthDogButton = allDogs[6]
+const seventhDogButton = allDogs[7]
+
+const firstDog = data[0]
+const secondDog = data[1]
+const thirdDog = data[2]
+const fourthDog = data[3]
+const fifthDog = data[4]
+const sixthDog = data[5]
+const seventhDog = data[6]
+
+const dogCard = document.querySelector('.main__dog-section')
+
+const dogName = document.querySelector('h2');
+dogCard.append(dogName)
+
+const dogImg = document.createElement('img')
+dogCard.append(dogImg)
+
+const dogBio = document.createElement('p')
+dogCard.append(dogBio)
+
+const naughtyCheck = document.createElement('p')
+dogCard.append(naughtyCheck)
+dogCard.setAttribute("class", "main__dog-section")
+
+const naughtyButton = document.createElement('button')
+dogCard.append(naughtyButton)
+naughtyButton.style.backgroundColor = 'white'
+
+firstDogButton.addEventListener("click", mrBonkers);
+
+function mrBonkers() {
+    dogName.innerText = firstDog.name
+    dogBio.innerText = bio
+    dogBio.setAttribute("class", "main__dog-section")
+    dogImg.src = firstDog.image
+    if (firstDog.isGoodDog===true) { 
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> No!!</p>"
+        naughtyButton.innerText = "Bad dog"
+} else if (firstDog.isGoodDog===false) {
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> Yes!!</p>"
+        naughtyButton.innerText = "Good dog"
+}
+}
+
+secondDogButton.addEventListener("click", nugget);
+
+function nugget() {
+    dogName.innerText = secondDog.name
+    dogBio.innerText = bio
+    dogBio.setAttribute("class", "main__dog-section")
+    dogImg.src = secondDog.image
+    if (secondDog.isGoodDog===true) { 
+            naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> No!!</p>"
+    } else if (secondDog.isGoodDog===false) {
+            naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> Yes!!</p>"
+    }
+    if (naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> Yes!!</p>") {
+        naughtyButton.innerText = "Good dog"
+        
+    } else if (naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> No!!</p>") {
+        naughtyButton.innerText = "Bad dog"
+    }
+}
+
+thirdDogButton.addEventListener("click", skittles);
+
+function skittles() {
+    dogName.innerText = thirdDog.name
+    dogBio.innerText = bio
+    dogBio.setAttribute("class", "main__dog-section")
+    dogImg.src = thirdDog.image
+    if (thirdDog.isGoodDog===true) { 
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> No!!</p>"
+} else if (thirdDog.isGoodDog===false) {
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> Yes!!</p>"
+}
+}
+
+fourthDogButton.addEventListener("click", buttercup);
+
+function buttercup() {
+    dogName.innerText = fourthDog.name
+    dogBio.innerText = bio
+    dogBio.setAttribute("class", "main__dog-section")
+    dogImg.src = fourthDog.image
+    if (fourthDog.isGoodDog===true) { 
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> No!!</p>"
+} else if (fourthDog.isGoodDog===false) {
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> Yes!!</p>"
+}
+}
+
+fifthDogButton.addEventListener("click", lucipher);
+
+function lucipher() {
+    dogName.innerText = fifthDog.name
+    dogBio.innerText = bio
+    dogBio.setAttribute("class", "main__dog-section")
+    dogImg.src = fifthDog.image
+    if (fifthDog.isGoodDog===true) { 
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> No!!</p>"
+} else if (fifthDog.isGoodDog===false) {
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> Yes!!</p>"
+}
+}
+
+sixthDogButton.addEventListener("click", snooper);
+
+function snooper() {
+    dogName.innerText = sixthDog.name
+    dogBio.innerText = bio
+    dogBio.setAttribute("class", "main__dog-section")
+    dogImg.src = sixthDog.image
+    if (sixthDog.isGoodDog===true) { 
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> No!!</p>"
+} else if (sixthDog.isGoodDog===false) {
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> Yes!!</p>"
+}
+}
+
+seventhDogButton.addEventListener("click", puddles);
+
+function puddles() {
+    dogName.innerText = seventhDog.name
+    dogBio.innerText = bio
+    dogBio.setAttribute("class", "main__dog-section")
+    dogImg.src = seventhDog.image
+    if (seventhDog.isGoodDog===true) { 
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> No!!</p>"
+} else if (seventhDog.isGoodDog===false) {
+        naughtyCheck.innerHTML = "<p><em>Is dog naughty?<em> Yes!!</p>"
+}
+}
