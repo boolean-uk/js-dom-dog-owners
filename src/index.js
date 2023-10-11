a985a6dfca34d5771629f7b1be797a41bede2f45
Rebecca Noy <rebecca.noy@gmx.de>
2023-10-11 15:47:49 +0200

    renderDogCard(), tested with static data set, wrote fillNav()

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ console.log(data);
 
 // WRITE YOUR CODE BELOW!
 const findDogObj = (name) => {
-  return data.find(val => val.name === name)
+  const foundDog = data.find(val => val.name === name)
+  console.log(foundDog)
+  return foundDog
 }
 
 const clearMain = () => {
@@ -10,6 +12,59 @@ const clearMain = () => {
   content.forEach((val) => val.remove())
 }
 
-const renderDogCard = (dog) => {
-  
+const fillNav = () => {
+  const nav = document.querySelector("header > ul")
+  console.log(nav)
+  data.forEach(dog => {
+    const li = document.createElement("li")
+    li.setAttribute("class", "dogs-list__button")
+    li.innerText = dog. name
+    nav.append(li)
+  })
 }
+
+const fillMain = (element) => {
+  const main = document.querySelector("main")
+}
+
+const renderDogCard = (dog) => {
+  const main = document.querySelector("main")
+
+  const card = document.createElement("section")
+  card.setAttribute("class", "main__dog-section")
+  const name = document.createElement("h2")
+  name.innerText = dog.name
+  card.appendChild(name)
+
+  const img = document.createElement("img")
+  img.setAttribute("src", dog.image)
+  card.appendChild(img)
+
+  const div = document.createElement("div")
+  div.setAttribute("class", "main__dog-section__desc")
+  card.appendChild(div)
+
+  const bio = document.createElement("h3")
+  bio.innerText = "Bio"
+  bioText = document.createElement("p")
+  bioText.innerText = dog.bio
+  div.appendChild(bio)
+  div.appendChild(bioText)
+
+  const p = document.createElement("p")
+  const eval = dog.isGoodDog ? "No" : "Yes"
+  p.innerHTML = `<em>Is naughty?</em> ${eval}!`
+  card.appendChild(p)
+
+  const button = document.createElement("button")
+  button.innerText = "Good Dog!"
+
+  card.appendChild(button)
+
+  main.appendChild(card)
+}
+
+clearMain()
+fillNav()
+
+fillMain(renderDogCard(data[0]))
