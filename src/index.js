09a8a0b7a7c27e3709fe5a13572a57854a4119b2
Kye <NovusAa12@proton.me>
2023-10-11 18:14:24 +0100

    add create-a-dog (part 2)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
-console.log(data);
-data[1].bio = "This is nugget, he doesn't have a bio because he is a bad dog.";
-
 // WRITE YOUR CODE BELOW!
+data[1].bio = "This is nugget, he doesn't have a bio because he is a bad dog.";
+//Card
+const dogList = document.querySelector(".dogs-list");
+
 function createDogList(data) {
-    const dogList = document.querySelector(".dogs-list");
+    const addDog = document.querySelector(".dogs-list__button--add");
+    addDog.addEventListener("click", renderForm)
 
     data.forEach(dog => {
         const list = document.createElement("li");
+
         list.setAttribute("class","dogs-list__button");
         list.innerText = dog.name;
         list.addEventListener("click", () => renderDogCard(dog));
 
         dogList.append(list);
     });
+};
 
+function updateDogList(dog) {
+    const list = document.createElement("li");
+
+    list.setAttribute("class","dogs-list__button");
+    list.innerText = dog.name;
+    list.addEventListener("click", () => renderDogCard(dog));
+
+    dogList.append(list);
+};
+
+function createSection() {
+    const section = document.createElement("section");
+    section.setAttribute("class", "main__dog-section");
+
+    return section
 };
 
 function createDogCardDesc(dog) {
@@ -31,8 +50,7 @@ function createDogCardDesc(dog) {
 
 function createDogCard(dog) {
     const main = document.querySelector(".main");
-    const section = document.createElement("section");
-    section.setAttribute("class", "main__dog-section");
+    const section = createSection();
 
     //Top Half
     const h2 = document.createElement("h2");
@@ -55,14 +73,99 @@ function createDogCard(dog) {
 
     //Append Changes
     section.append(h2, img, desc, p, button);
-    main.append(section)
+    main.append(section);
 };
 
 function renderDogCard(dog) {
     const noDogs = document.querySelector(".main__dog-section");
-    const dogCard = createDogCard(dog);
 
-    noDogs ? (noDogs.remove(), dogCard): noDogs;
+    noDogs ? (noDogs.remove(), createDogCard(dog)): noDogs;
+};
+
+//Form
+function createForm() {
+    const form = document.createElement("form");
+    form.setAttribute("class", "form");
+
+    //Input Name
+    const labelName = document.createElement("label");
+    labelName.setAttribute("for", "name");
+    const inputName = document.createElement("input");
+    inputName.setAttribute("type", "text");
+    inputName.setAttribute("name", "name");
+    inputName.setAttribute("id", "name");
+
+    //Input Pic
+    const labelPic = document.createElement("label");
+    labelPic.setAttribute("for", "name");
+    const inputPic = document.createElement("input");
+    inputPic.setAttribute("type", "url");
+    inputPic.setAttribute("name", "image");
+    inputPic.setAttribute("id", "image");
+
+    //Input Bio
+    const labelBio = document.createElement("label");
+    labelBio.setAttribute("for", "name");
+    const inputBio = document.createElement("textarea");
+    inputBio.setAttribute("rows", "5");
+    inputBio.setAttribute("name", "bio");
+    inputBio.setAttribute("id", "bio");
+
+    //Submit
+    const submit = document.createElement("input");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("id", "submit");
+    submit.setAttribute("name", "submit");
+    submit.setAttribute("value", "Let's add a dog!");
+    submit.setAttribute("class", "form__button");
+
+    form.addEventListener("submit", submitForm)
+
+    form.append(
+        labelName, 
+        inputName,
+        labelPic,
+        inputPic,
+        labelBio,
+        inputBio, 
+        submit
+        );
+    
+    return form
+};
+
+function createFormCard() {
+    const main = document.querySelector(".main");
+    const section = createSection();
+    const form = createForm();
+
+    const h2 = document.createElement("h2");
+    h2.innerText = "Add a new Dog";
+
+    section.append(h2, form)
+    main.append(section)
+};
+
+function renderForm() {
+    const noDogs = document.querySelector(".main__dog-section");
+
+    noDogs ? (noDogs.remove(), createFormCard()): noDogs;
+};
+
+//Submit Form 
+function submitForm(e) {
+    e.preventDefault();
+
+    const newDog = {
+        id: data.length +1,
+        bio: e.target.bio.value,
+        image: e.target.image.value,
+        isGoodDog: true,
+        name: e.target.name.value
+    };
+
+    data.push(newDog);
+    updateDogList(newDog);
 };
 
 createDogList(data)
