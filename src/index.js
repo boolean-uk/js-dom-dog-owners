f2376d3cd1c99785c116b9ce01cd66fc75d8795e
Rebecca Noy <rebecca.noy@gmx.de>
2023-10-11 15:20:41 +0200

    wrote clearing function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
+const findDogObj = (name) => {
+  return data.find(val => val.name === name)
+}
+
+const clearMain = () => {
+  const content = document.querySelectorAll("main > *")
+  content.forEach((val) => val.remove())
+}
+
+const renderDogCard = (dog) => {
+  
+}
