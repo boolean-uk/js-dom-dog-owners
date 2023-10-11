b1722945dc5aa248db12c3b0cf58b036a8a72ad3
Nazar Tymiv <nazartumiv@gmail.com>
2023-10-11 13:17:44 +0100

    Part 1 has been finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 console.log(data);
+const dogList = document.querySelector(".dogs-list");
+const main = document.querySelector(".main");
 
-// WRITE YOUR CODE BELOW!
+data.forEach((dog) => {
+    dogList.innerHTML += `<li class="dogs-list__button">${dog.name}</li> `;
+});
+
+const navigation__buttons = document.querySelectorAll(".dogs-list__button");
+
+const render = (res) => {
+    const element = `
+    <section class="main__dog-section">
+        <h2>${res.name}</h2>
+        <img
+        src="${res.image}"
+        alt="dog"
+        />
+        <div class="main__dog-section__desc">
+            <h3>Bio</h3>
+            <p>
+                ${res.bio}
+            </p>
+        </div>
+        <p><em>Is naughty?</em> ${res.isGoodDog ? "No!" : "Yes!"}</p>
+        <button>${res.isGoodDog ? "Good dog!" : "Bad Dog!"}</button>
+    </section> 
+    `;
+
+    main.innerHTML = element;
+};
+
+navigation__buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const res = data.find((i) => i.name === button.innerHTML);
+        render(res);
+    });
+});
