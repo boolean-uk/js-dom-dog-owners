afd4f781e26b7d3dd591a86e2d3c979998338127
Lukas Dembicki <lad_777@hotmail.com>
2023-10-11 18:03:00 +0100

    Mostly working. Almost there. Good dog button left

--- src/index.js
+++ src/index.js
@@ -1,3 +1,86 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
+
+const yesOrNo = (data) => {
+    let answer = "";
+    if (data.isGoodDog === true) {
+        answer = "Yes!";
+    } else if (data.isGoodDog === false) {
+        answer = "No!";
+    }
+    return answer;
+};
+
+const goodBad = (data) => {
+    let answer = "";
+    if (data.isGoodDog === true) {
+        answer = "Good Dog!";
+    } else if (data.isGoodDog === false) {
+        answer = "Bad Dog!";
+    }
+    return answer;
+};
+
+
+const dogButtonList = document.querySelector(".dogs-list");
+const main = document.querySelector(".main");
+
+
+const removeDogs = () => {
+    const allContent = document.querySelectorAll(".main > *")
+    allContent.forEach((val) => val.remove())
+}
+
+
+data.forEach((dogName) => {
+    const dogButton = document.createElement("li");
+    dogButton.innerText = `${dogName.name}`;
+    dogButton.setAttribute("class", "dogs-list__button");
+    dogButton.setAttribute("Id", `${dogName.name}`);
+    dogButtonList.append(dogButton);
+
+    dogButton.addEventListener("click", () => {
+        removeDogs()
+        createDog(dogName)
+    });
+});
+
+const createDog = (dogCard) => {
+    const mainDogSection = document.createElement("section");
+    mainDogSection.setAttribute("class", "main__dog-section");
+    main.append(mainDogSection);
+
+    const dogName = document.createElement("h2");
+    dogName.innerText = `${dogCard.name}`;
+    mainDogSection.append(dogName);
+
+    const dogImg = document.createElement("img");
+    dogImg.src = `${dogCard.image}`;
+    dogImg.alt = `${dogCard.name}`;
+    mainDogSection.append(dogImg);
+
+    const dogBioSection = document.createElement("div");
+    dogBioSection.setAttribute("class", "main__dog-section__desc");
+    mainDogSection.append(dogBioSection);
+
+    const bio = document.createElement("h3");
+    bio.innerText = "Bio";
+    dogBioSection.append(bio);
+
+    const p = document.createElement("p");
+    p.innerText = `${dogCard.bio}`;
+    dogBioSection.append(p);
+
+    const pOne = document.createElement("p");
+    pOne.innerHTML = `<em> Is naughty? <em> ${yesOrNo(dogCard)}`;
+    mainDogSection.append(pOne);
+
+    const button = document.createElement("button");
+    button.innerText = `${goodBad(dogCard)}`;
+    mainDogSection.append(button);
+    button.addEventListener("click", () => {
+        dogCard.isGoodDog = !dogCard.isGoodDog;
+        console.log(data);
+    });
+};
