08362a70a5bac4c3074288a885e50bfcfe6a8ac0
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-10-11 15:16:40 +0100

    initial draft

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-console.log(data);
+const dogsData = data
 
 // WRITE YOUR CODE BELOW!
+
+// populate .dogs-list with dog buttons
+  //access dogs = data
+  //do a forEach((dog) => {}):
+    //for each dog we want dog.name
+    //append li item with class "dogs-list__button"
+
+
+const navListItem = document.createElement("li")
+navListItem.innerText = "Hello"
+
+const dogsList = document.querySelector(".dogs-list")
+
+dogsList.append(navListItem)
