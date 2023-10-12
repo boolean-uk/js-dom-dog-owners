b1e65ef3bcb314aefa640eff0078651489be56a7
Pierluigi <pierluigi.capirci89@gmail.com>
2023-10-12 12:36:28 +0100

    created list and Bio

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 console.log(data);
 
-// WRITE YOUR CODE BELOW!
+const dogList = document.querySelector(".dogs-list");
+
+const dogListItem = data.forEach((dog) => {
+  const list = document.createElement("li");
+  list.innerText = dog.name;
+  dogList.append(list);
+  list.setAttribute("class", "dogs-list__button ");
+
+  list.addEventListener("click", () => {
+    const dogCard = document.querySelector(".main__dog-section");
+    const dogDetails = document.querySelectorAll("section > *");
+    dogDetails.forEach((element) => element.remove());
+
+    const dogName = document.createElement("h2");
+    dogName.innerText = dog.name;
+    dogCard.append(dogName);
+
+    const dogImage = document.createElement("img");
+    dogImage.setAttribute("src", dog.image);
+    dogImage.setAttribute("alt", dog.name);
+    dogImage.setAttribute("width", "400px");
+    dogImage.setAttribute("height", "400px");
+    dogCard.append(dogImage);
+    
+    const bioH3 = document.createElement("h3");
+    bioH3.innerText = "Bio";
+    dogCard.append(bioH3);
+
+    const dogBio = document.createElement("p");
+    dogBio.innerText = dog.bio;
+    dogCard.append(dogBio);
+
+  });
+});
