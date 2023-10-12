ae21712b2e5ce5a354a3612e6d6e5d5c47f4dabe
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-10-12 22:51:09 +0100

    moved a few things over

--- src/index.js
+++ src/index.js
@@ -2,61 +2,52 @@ console.log(data);
 
 // WRITE YOUR CODE BELOW!
 
-const dogList = document.querySelector(".dogs-list");
+document.addEventListener("DOMContentLoaded", function () {
+  const dogList = document.querySelector(".dogs-list");
+  const mainDogSection = document.querySelector(".main__dog-section");
 
-const mainDogSection = document.querySelector(".main__dog-section");
+  function renderDogList() {
+    data.forEach((dog) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = dog.name;
+      listItem.className = "dogs-list__button";
 
-function renderDogList() {
-  data.forEach((dog) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = dog.name;
-    listItem.className = "dogs-list__button";
+      listItem.addEventListener("click", () => {
+        renderMainDog(dog);
+      });
 
-    listItem.addEventListener("click", () => {
-      renderMainDog(dog);
+      dogList.appendChild(listItem);
     });
-
-    dogList.appendChild(listItem);
-  });
-}
-
-function renderMainDog(dog) {
-  while (mainDogSection.firstChild) {
-    mainDogSection.removeChild(mainDogSection.firstChild);
   }
 
-  const dogInfo = document.createElement("div");
-  dogInfo.className = "dog-info";
+  function renderMainDog(dog) {
+    while (mainDogSection.firstChild) {
+      mainDogSection.removeChild(mainDogSection.firstChild);
+    }
 
-  const image = document.createElement("img");
-  image.src = dog.image;
-  image.alt = dog.name;
+    const dogInfo = document.createElement("div");
+    dogInfo.className = "dog-info";
 
-  const name = document.createElement("h2");
-  name.textContent = dog.name;
+    const image = document.createElement("img");
+    image.src = dog.image;
+    image.alt = dog.name;
 
-  const bio = document.createElement("p");
-  bio.textContent = dog.bio;
+    const name = document.createElement("h2");
+    name.textContent = dog.name;
 
-  const isGoodDog = document.createElement("p");
-  isGoodDog.textContent = dog.isGoodDog ? "Good Dog" : "Not a Good Dog";
+    const bio = document.createElement("p");
+    bio.textContent = dog.bio;
 
-  dogInfo.appendChild(image);
-  dogInfo.appendChild(name);
-  dogInfo.appendChild(bio);
-  dogInfo.appendChild(isGoodDog);
+    const isGoodDog = document.createElement("p");
+    isGoodDog.textContent = dog.isGoodDog ? "Good Dog" : "Not a Good Dog";
 
-  mainDogSection.appendChild(dogInfo);
-}
+    dogInfo.appendChild(image);
+    dogInfo.appendChild(name);
+    dogInfo.appendChild(bio);
+    dogInfo.appendChild(isGoodDog);
 
-renderDogList();
+    mainDogSection.appendChild(dogInfo);
+  }
 
-
-const plusButton = document.querySelector(".dogs-list__button--add");
-
-// Get the form and input elements for adding a new dog
-const addDogForm = document.querySelector(".add-dog-form");
-const nameInput = document.querySelector("#dog-name");
-const bioInput = document.querySelector("#dog-bio");
-const isGoodDogInput = document.querySelector("#is-good-dog");
-
+  renderDogList();
+});
