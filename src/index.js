961d7a3d4aae6cb61ebaaefec42852009cc3c88a
TomEastwood <t.eastwood99@gmail.com>
2023-10-12 17:09:04 +0100

    core part 1 completed

--- src/index.js
+++ src/index.js
@@ -5,39 +5,39 @@ const body = document.querySelector('.body');
 const container = document.querySelector('.main');
 const dogList = document.querySelector('.dogs-list');
 
-function dogID(data) {
+function dogID(dog) {
     const dogID = document.createElement('h2');
     dogID.setAttribute('class', 'main__dog-id');
-    dogID.setAttribute('id', `${data.id}`);
+    dogID.setAttribute('id', `${dog.id}`);
     dogID.innerText = "";
     return dogID;
 }
 
-function dogName(data) {
+function dogName(dog) {
     const dogName = document.createElement('h2');
     dogName.setAttribute('class', 'main__dog-name');
-    dogName.innerText = `${data.name}`;
+    dogName.innerText = `${dog.name}`;
     return dogName;
 };
 
-function dogImage(data) {
+function dogImage(dog) {
     const dogImage = document.createElement('img');
     dogImage.setAttribute('class', 'main__dog-image');
-    dogImage.src = `${data.image}`;
+    dogImage.src = `${dog.image}`;
     return dogImage;
 };
 
-function dogBio(data) {
+function dogBio(dog) {
     const dogBio = document.createElement('p');
     dogBio.setAttribute('class', 'main__dog-p');
-    dogBio.innerText = `${data.bio}`;
+    dogBio.innerText = `${dog.bio}`;
     return dogBio;
 };
 
-function isGoodDog(data) {
+function isGoodDog(dog) {
     const dogGood = document.createElement('p');
     dogGood.setAttribute('class', 'main__dog-p');
-    if (data.isGoodDog === true) {
+    if (dog.isGoodDog === true) {
         dogGood.innerText = 'Good Dog!';
     } else {
         dogGood.innerText = 'Bad Dog!';
@@ -45,18 +45,24 @@ function isGoodDog(data) {
     return dogGood;
 };
 
-function dogButton(data) {
+function dogButton(dog) {
     const dogButton = document.createElement('button');
-    dogButton.setAttribute('class', 'dog-list__button');
-    if (data.isGoodDog === true) {
-        dogButton.innerText = 'Bad Dog!';
+    dogButton.setAttribute('class', 'main__dog-section button');
+    if (dog.isGoodDog === true) {
+        dogButton.innerText = 'Set Bad Dog!';
     } else {
-        dogButton.innerText = 'Good Dog!';
+        dogButton.innerText = 'Set Good Dog!';
     }
+    dogButton.addEventListener('click', () => {
+        dog.isGoodDog = !dog.isGoodDog
+        container.innerHTML = ''
+        createDogCard(dog)
+        console.log(dog.isGoodDog);
+    });
     return dogButton;
 };
 
-data.forEach(dog => {
+function createDogCard(dog) {
     const dogDiv = document.createElement('div');
     dogDiv.setAttribute('class', 'main__dog-section');
     dogDiv.append(dogID(dog))
@@ -66,11 +72,24 @@ data.forEach(dog => {
     dogDiv.append(isGoodDog(dog))
     dogDiv.append(dogButton(dog))
     container.append(dogDiv);
+}
 
-    const dogs = document.createElement('li');
-    dogs.setAttribute('class', 'dogs-list__button');
-    dogs.innerText = `${dog.name}`;
-    dogList.append(dogs);
-});
-console.log(container);
+function createNavButtons () {
+    data.forEach(dog => {
+        const dogs = document.createElement('li');
+        dogs.setAttribute('class', 'dogs-list__button');
+        dogs.innerText = `${dog.name}`;
+        dogList.append(dogs);
+        dogs.addEventListener('click', () => {
+            console.log('dog clicked')
+            container.innerHTML = ''
+            createDogCard(dog)
+        })
+    });
+}
+
+createNavButtons()
+
+
+
 
