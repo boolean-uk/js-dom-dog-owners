70dca6b54e2fc002911a1e10de2cdc2e159492ea
Satoki <satokiito@gmail.com>
2023-10-12 17:57:49 +0100

    removed inner HTML, changed some functions

--- src/index.js
+++ src/index.js
@@ -1,91 +1,110 @@
 // WRITE YOUR CODE BELOW!
 
 // LIST OF DOGGOS
-const dogList = document.querySelector('header > ul')
+const dogList = document.querySelector('header > ul');
 
 // CREATE HEADER DOGGO LIST
 const dogListItem = data.forEach(dog => {
-    const dogListCard = document.createElement('li')
-    dogListCard.innerText = dog.name
-    dogList.append(dogListCard)
+    const dogListCard = document.createElement('li');
+    dogListCard.innerText = dog.name;
+    dogList.append(dogListCard);
 
-    dogListCard.setAttribute('class', 'dogs-list__button ')
+    dogListCard.setAttribute('class', 'dogs-list__button ');
 
     // CLICK EVENT LISTENER FUNCTION
     dogListCard.addEventListener('click', () => {
 
         // DEFINE SECTION TO APPEND TO
-        const dogDisplayCard = document.querySelector('section')
+        const dogDisplayCard = document.querySelector('section');
 
         // REMOVE PREVIOUS DETAILS ON CLICK
-        const dogDisplayDetails = document.querySelectorAll('section > *')
-        dogDisplayDetails.forEach((element) => element.remove())
-        
+        const dogDisplayDetails = document.querySelectorAll('section > *');
+        dogDisplayDetails.forEach((element) => element.remove());
+
         // ADD NAME
-        const dogDisplayName = document.createElement('h2')
-        dogDisplayName.innerText = dog.name
-        dogDisplayCard.append(dogDisplayName)
+        const dogDisplayName = document.createElement('h2');
+        dogDisplayName.innerText = dog.name;
+        dogDisplayCard.append(dogDisplayName);
            
         // ADD PHOTO
-        const dogDisplayImage = document.createElement('img')
-        dogDisplayImage.src = dog.image
-        dogDisplayImage.alt = `${dog.name} photo`
-        dogDisplayImage.width = '420'
-        dogDisplayImage.height = '420'
-        dogDisplayCard.append(dogDisplayImage)
+        const dogDisplayImage = document.createElement('img');
+        dogDisplayImage.src = dog.image;
+        dogDisplayImage.alt = `${dog.name} photo`;
+        dogDisplayImage.width = '420';
+        dogDisplayImage.height = '420';
+        dogDisplayCard.append(dogDisplayImage);
 
         // ADD BIO  
-        const bioHeader = document.createElement('h3')
-        bioHeader.innerText = 'Bio'
-        dogDisplayCard.append(bioHeader)
+        const bioHeader = document.createElement('h3');
+        bioHeader.innerText = 'Bio';
+        dogDisplayCard.append(bioHeader);
 
-        const dogDisplayBio = document.createElement('p')
-        dogDisplayBio.innerText = dog.bio
-        dogDisplayCard.append(dogDisplayBio)
+        const dogDisplayBio = document.createElement('p');
+        dogDisplayBio.innerText = dog.bio;
+        dogDisplayCard.append(dogDisplayBio);
 
         // MAIN SECTION BOTTOM
-        const dogDisplayBottom = document.createElement('div')
-        dogDisplayBottom.setAttribute('class', 'main__dog-section__btn')
+        const dogDisplayBottom = document.createElement('div');
+        dogDisplayBottom.setAttribute('class', 'main__dog-section__btn');
         
         // ADD GOOD OR BAD BOI
-        const dogDisplayGoodBad = document.createElement('p')
-        dogDisplayGoodBad.setAttribute('class', 'main__dog-section__desc')
-
-        if (dog.isGoodDog === true) {
-            dogDisplayGoodBad.innerHTML = `<em>is good boi?</em> Yes!`
+        const question = (dog) => {
+            const questionString = document.createElement('p');
+            questionString.setAttribute('class', 'main__dog-section__desc');
+    
+            const em = document.createElement('em'); 
+            em.innerText = 'is good boi?';
+    
+            const checkIfGood = (dog) => {
+                let yesOrNo = '';
+                if (dog.isGoodDog) {
+                    yesOrNo = ' Yes!';
+                }
+                else {
+                    yesOrNo = ' No';
+                }
+                return yesOrNo;
+            }
+            questionString.append(em, checkIfGood(dog));
+            return questionString;
         }
-        else {
-            dogDisplayGoodBad.innerHTML = `<em>is good boi?</em> No:(`
-        }
-        dogDisplayBottom.append(dogDisplayGoodBad)
-        dogDisplayCard.append(dogDisplayBottom)
-
-
+            dogDisplayBottom.append(question(dog));
+     
         // ADD GOOD/BAD BOI BUTTON
-        const goodBoiButton = document.createElement('button')
-        const goodBoiButtonText = document.createElement('p')
-
-        if (dog.isGoodDog === true) {
-            goodBoiButtonText.innerText = 'Bad Doggo'
+        const button = (dog) => {
+            const buttonElement = document.createElement('button');
+            const goodBoiButtonText = document.createElement('p');
+    
+            if (dog.isGoodDog) {
+                goodBoiButtonText.innerText = 'Bad Doggo';
+                dog.isGoodDog = false;
+            }
+            else {
+                goodBoiButtonText.innerText = 'Good Doggo';
+                dog.isGoodDog = true;
+            }
+            buttonElement.append(goodBoiButtonText);
+            
+            return buttonElement;
         }
-        else {
-            goodBoiButtonText.innerText = 'Good Doggo'
-        }
+        const goodBoiButton = button(dog);
+        dogDisplayBottom.append(goodBoiButton);
 
-        goodBoiButton.append(goodBoiButtonText)
-        dogDisplayBottom.append(goodBoiButton)
-        dogDisplayCard.append(dogDisplayBottom)
+        // APPEND BOTTOM DISPLAY TO CARD
+        dogDisplayCard.append(dogDisplayBottom);
 
         // CHANGE BUTTON DISPLAY ON CLICK
         goodBoiButton.addEventListener('click', () => {
 
-            if (goodBoiButtonText.innerText === 'Bad Doggo') {
-                goodBoiButtonText.innerText = 'Good Doggo'
-                dogDisplayGoodBad.innerHTML = `<em>is good boi?</em> No:(`   
+        const dogDisplayBottomContent = dogDisplayBottom.querySelectorAll('div > *');
+
+            if (dog.isGoodDo === true) {
+                dogDisplayBottomContent.forEach((element) => element.remove());
+                dogDisplayBottom.append(question(dog), button(dog));
             }       
             else  {
-                goodBoiButtonText.innerText = 'Bad Doggo'
-                dogDisplayGoodBad.innerHTML = `<em>is good boi?</em> Yes!`             
+                dogDisplayBottomContent.forEach((element) => element.remove());
+                dogDisplayBottom.append(question(dog), button(dog));
             }
         })
     })
