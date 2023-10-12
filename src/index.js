a2e0e5466bd73c99343c7f573cde3fda02d93c7f
Nazar Tymiv <nazartumiv@gmail.com>
2023-10-12 12:07:02 +0100

    Fixed inner html

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const dogList = document.querySelector(".dogs-list");
 const main = document.querySelector(".main");
 
 const renderNavigation = () => {
-    dogList.innerHTML =
-        '<li class="dogs-list__button dogs-list__button--add">+</li>';
+    const add = document.createElement("li");
+    add.setAttribute("class", "dogs-list__button dogs-list__button--add");
+    add.innerText = "+";
+    dogList.innerText = "";
+    dogList.append(add);
+
     newData.forEach((dog) => {
-        dogList.innerHTML += `<li class="dogs-list__button">${dog.name}</li> `;
+        const li = document.createElement("li");
+        li.innerText = dog.name;
+        li.setAttribute("class", "dogs-list__button");
+        dogList.append(li);
     });
 
     const navigation__buttons = document.querySelectorAll(".dogs-list__button");
@@ -22,25 +29,60 @@ const renderNavigation = () => {
     const addButton = document.querySelector(".dogs-list__button--add");
 
     addButton.addEventListener("click", () => {
-        const element = `
-            <section class="main__dog-section">
-                <h2>Add a new Dog</h2>
-                <form class="form">
-                    <label for="name">Dog's name</label>
-                    <input type="text" id="name" name="name">
+        const section = document.createElement("section");
+        section.setAttribute("class", "main__dog-section");
 
-                    <label for="image">Dog's picture</label>
-                    <input type="url" id="image" name="image">
+        const formTitle = document.createElement("h2");
+        formTitle.innerText = "Add a new Dog";
 
-                    <label for="bio">Dog's bio</label>
-                    <textarea rows="5" id="bio" name="bio"></textarea>
+        const formBlock = document.createElement("form");
+        formBlock.setAttribute("class", "form");
 
-                    <input type="submit" id="submit" name="submit" value="Let's add a dog!" class="form__button">
-                </form>
-            </section>
-        `;
+        const labelName = document.createElement("label");
+        labelName.setAttribute("for", "name");
+        labelName.innerText = "Dog's name";
+        const inputName = document.createElement("input");
+        inputName.setAttribute("type", "text");
+        inputName.setAttribute("id", "name");
+        inputName.setAttribute("name", "name");
 
-        main.innerHTML = element;
+        const labelPic = document.createElement("label");
+        labelPic.setAttribute("for", "image");
+        labelPic.innerText = "Dog's picture";
+        const inputPic = document.createElement("input");
+        inputPic.setAttribute("type", "url");
+        inputPic.setAttribute("id", "image");
+        inputPic.setAttribute("name", "image");
+
+        const labelBio = document.createElement("label");
+        labelBio.setAttribute("for", "bio");
+        labelBio.innerText = "Dog's bio";
+        const inputBio = document.createElement("textarea");
+        inputBio.setAttribute("rows", "5");
+        inputBio.setAttribute("id", "bio");
+        inputBio.setAttribute("name", "bio");
+
+        const submitButton = document.createElement("input");
+        submitButton.setAttribute("type", "submit");
+        submitButton.setAttribute("id", "submit");
+        submitButton.setAttribute("name", "submit");
+        submitButton.setAttribute("value", "Let's add a dog!");
+        submitButton.setAttribute("class", "form__button");
+
+        // configuration
+        section.append(formTitle, formBlock);
+        formBlock.append(
+            labelName,
+            inputName,
+            labelPic,
+            inputPic,
+            labelBio,
+            inputBio,
+            submitButton
+        );
+
+        main.innerText = "";
+        main.append(section);
 
         const form = document.querySelector(".form");
         form.addEventListener("submit", (e) => {
@@ -62,27 +104,45 @@ const renderNavigation = () => {
 };
 
 const render = (res) => {
-    const element = `
-        <section class="main__dog-section">
-            <h2>${res.name}</h2>
-            <img
-            src="${res.image}"
-            alt="dog" style="width: 350px; height: 300px;"
-            />
-            <div class="main__dog-section__desc">
-                <h3>Bio</h3>
-                <p>
-                    ${res.bio}
-                </p>
-            </div>
-            <p><em>Is naughty?</em> ${res.isGoodDog ? "No!" : "Yes!"}</p>
-            <button onClick=toggleDogStatus(${res.id + "," + res.isGoodDog})>${
-        res.isGoodDog ? "Good dog!" : "Bad Dog!"
-    }</button>
-        </section> 
-    `;
+    const section = document.createElement("section");
+    section.setAttribute("class", "main__dog-section");
 
-    main.innerHTML = element;
+    const dogTitle = document.createElement("h2");
+    dogTitle.innerText = res.name;
+
+    const dogImg = document.createElement("img");
+    dogImg.src = res.image;
+    dogImg.setAttribute("alt", "dog");
+    dogImg.setAttribute("style", "width: 350px; height: 300px;");
+
+    const dogBio = document.createElement("div");
+    dogBio.setAttribute("class", "main__dog-section__desc");
+
+    const dogBioTitle = document.createElement("h3");
+    dogBioTitle.innerText = "Bio";
+
+    const dogBioParagraph = document.createElement("p");
+    dogBioParagraph.innerText = res.bio;
+
+    const dogIsNaughty = document.createElement("p");
+    const em = document.createElement("em");
+    em.innerText = "Is naughty?";
+    dogIsNaughty.append(em);
+    dogIsNaughty.innerText += `${res.isGoodDog ? " No!" : " Yes!"}`;
+
+    const isGoodDogBlock = document.createElement("button");
+    isGoodDogBlock.setAttribute(
+        "onClick",
+        `toggleDogStatus(${res.id + "," + res.isGoodDog})`
+    );
+    isGoodDogBlock.innerText = `${res.isGoodDog ? "Good dog!" : "Bad Dog!"}`;
+
+    // configuration
+    section.append(dogTitle, dogImg, dogBio, dogIsNaughty, isGoodDogBlock);
+    dogBio.append(dogBioTitle, dogBioParagraph);
+
+    main.innerText = "";
+    main.append(section);
 };
 
 const toggleDogStatus = (id, status) => {
