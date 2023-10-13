8dac0929ba564aeba07d77c584fb89808f632fdf
Lukas Dembicki <lad_777@hotmail.com>
2023-10-13 17:49:24 +0100

    Almost finsihed. Need to get add button working.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ console.log(data);
 
 // WRITE YOUR CODE BELOW!
 
+// Function to change boolean of if the dog is good or bad//
 const goodBadDog = () => {
     data.isGoodDog = !data.isGoodDog;
 };
 
+// Function to create a string for the value of the boolean that determines a good or bad dog.//
 const yesOrNo = (data) => {
     let answer = "";
     if (data.isGoodDog === true) {
-        answer = "Yes!";
+        answer = " Yes!";
     } else if (data.isGoodDog === false) {
-        answer = "No!";
+        answer = " No!";
     }
     return answer;
 };
 
+// Function to create a string for a button for the value of the boolean that determines a good or bad dog.//
 const goodBad = (data) => {
     let answer = "";
     if (data.isGoodDog === true) {
@@ -26,15 +29,28 @@ const goodBad = (data) => {
     return answer;
 };
 
+// Some query selectors//
 const dogButtonList = document.querySelector(".dogs-list");
 const main = document.querySelector(".main");
 const addDog = document.querySelector(".dogs-list__button--add");
 
+// A function to remove dogs on the screen//
 const removeDogs = (value) => {
     const allContent = document.querySelectorAll(".main > *");
     allContent.forEach((value) => value.remove());
 };
 
+// A function to remove the list items from header
+const removeDogsNames = (value) => {
+    dogButtonList.innerHTML = "";
+    const li = document.createElement("li");
+    li.classList.add("dogs-list__button")
+    li.classList.add("dogs-list__button--add");
+    li.innerText = "+";
+    dogButtonList.append(li);
+};
+
+//Main function to create a dog card//
 const createDog = (dogCard) => {
     const mainDogSection = document.createElement("section");
     mainDogSection.setAttribute("class", "main__dog-section");
@@ -82,6 +98,7 @@ const createDog = (dogCard) => {
     });
 };
 
+// A function to create the nav bar with dogs//
 const createDogNav = (dogName) => {
     const dogButton = document.createElement("li");
     dogButton.innerText = `${dogName.name}`;
@@ -95,29 +112,37 @@ const createDogNav = (dogName) => {
     });
 };
 
+// The entire loop to render the dogs onto the page//
 data.forEach((value) => {
     createDogNav(value);
 });
 
-function createNewDog() {
+// A function to create a form for the new dog//
+function createNewDogForm() {
     removeDogs();
     const section = createSection();
     section.setAttribute("class", "main__dog-section");
 
+    // This creates an h2
     const h2 = createH2("Add a new Dog");
 
+    // This creates a form
     const form = createForm();
     form.setAttribute("class", "form");
 
+    // This creates first label and name input
     const labelName = createLabel("name", "Dog's name");
     const inputText = createInput("text", "name", "name", "", "");
 
+    // This creates label and src for dog image
     const labelImg = createLabel("image", "Dog's Picture");
     const inputUrl = createInput("url", "image", "image", "", "");
 
+    // This creates the label and textbox for a dog bio
     const labelBio = createLabel("bio", "Dog's Bio");
     const textArea = createTextArea("5", "bio", "bio");
 
+    // The submit button for adding a dog
     const inputButton = createInput(
         "submit",
         "submit",
@@ -126,6 +151,7 @@ function createNewDog() {
         "form__button"
     );
 
+    // All appends
     main.append(section);
     section.append(h2);
     section.append(form);
@@ -140,24 +166,57 @@ function createNewDog() {
     form.append(textArea);
 
     form.append(inputButton);
+
+    // query selects for adding to newDogState
+    const dogsName = document.querySelector("#name");
+    const dogsPic = document.querySelector("#image");
+    const dogsBio = document.querySelector("#bio");
+
+    // event listener for submitting new dog information.
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const newDog = {
+            id: data.length + 1,
+            name: dogsName.value,
+            bio: dogsBio.value,
+            isGoodDog: true,
+            image: dogsPic.value,
+        };
+
+        data.unshift(newDog);
+        removeDogsNames();
+        data.forEach((value) => {
+            createDogNav(value);
+        });
+
+        dogsName.value = '';
+        dogsPic.value = '';
+        dogsBio.value = '';
+        
+    });
 }
 
+//A function to create a Section//
 function createSection() {
     const section = document.createElement("section");
     return section;
 }
 
+// A function to create a string //
 function createH2(string) {
     const h2 = document.createElement("h2");
     h2.innerText = string;
     return h2;
 }
 
+// A function to create a form //
 function createForm() {
     const form = document.createElement("form");
     return form;
 }
 
+// A function to create a Label //
 function createLabel(target, string) {
     const label = document.createElement("label");
     label.setAttribute("for", target);
@@ -165,6 +224,7 @@ function createLabel(target, string) {
     return label;
 }
 
+// A function to create a Input //
 function createInput(type, id, name, value, classe) {
     const input = document.createElement("input");
     input.setAttribute("type", type);
@@ -175,6 +235,7 @@ function createInput(type, id, name, value, classe) {
     return input;
 }
 
+// A function to create a Text Area //
 function createTextArea(rows, id, name) {
     const textArea = document.createElement("textarea");
     textArea.setAttribute("rows", rows);
@@ -183,19 +244,7 @@ function createTextArea(rows, id, name) {
     return textArea;
 }
 
+// When the plus button gets clicked it will run a function that adds the new dog form onto screen//
 addDog.addEventListener("click", () => {
-    createNewDog();
+    createNewDogForm();
 });
-
-
-const newDogData = [
-    {
-        name: "",
-        bio: "",
-        isGoodDog: true,
-        image: "",
-    },
-]
-
-const submitted = document.querySelector('#submit');
-submitted.addEventListener("submit")
