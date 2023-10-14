fa7775c838fc5ee1bd5ea7280e7e1bd692604b52
aayushlama4008 <aayushlama4008@gmail.com>
2023-10-14 20:58:41 +0200

    done the part 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
-
+const boom =() =>{
 const dogList = document.querySelector('header > ul')
 
 //for loop
@@ -54,20 +54,21 @@ const dogList = document.querySelector('header > ul')
                     biodetails.innerText = `${dog.bio}`
                     div.append(biodetails)
                 
-                //for em text
+                 //for em text
                 const doggieStatus = document.createElement('p')
+                            
+                if(dog.isGoodDog === true){
+                    doggieStatus.innerText = 'Yes!'
+                }else{
+                    doggieStatus.innerText = 'No!'
                 
-                    if(dog.isGoodDog === true){
-                        doggieStatus.innerText = 'Yes!'
-                    }else{
-                        doggieStatus.innerText = 'No!'
-                    
-                    }
-                    const em = document.createElement('em')
-                    em.innerText = 'Is naughty?'
-                    doggieStatus.append(em)
-                    section.append(doggieStatus)
-
+                }
+                const em = document.createElement('em')
+                em.innerText = 'Is naughty?'
+                doggieStatus.append(em)
+                section.append(doggieStatus)
+                
+                
 
                 //for button
                 const button = document.createElement('button')
@@ -95,100 +96,126 @@ const dogList = document.querySelector('header > ul')
             });
         
     });
-   
+}
+boom(data)
 //for the form 
-const addForm = document.querySelector('.dogs-list__button--add')
+const addDog = () =>{
+    const addForm = document.querySelector('.dogs-list__button--add')
 
-const newForm = document.querySelector('section')
+    addForm.addEventListener('click', () => {
+        //to remove evrything inside the section
+        const toRemove = document.querySelectorAll('section > *')
+        toRemove.forEach(element => element.remove())
 
-addForm.addEventListener('click', () => {
-    //to remove evrything inside the section
-    const toRemove = document.querySelectorAll('section > *')
-    toRemove.forEach(element => element.remove())
+        const newFormSection = document.querySelector('section')
+        newFormSection.setAttribute("class", "main__dog-section")
 
-    //form title
-    const newDogHeader =  document.createElement('h2')
-    newDogHeader.innerText = 'Add a new Dog'
-    newForm.append(newDogHeader)
+        //form title
+        const newDogHeader =  document.createElement('h2')
+        newDogHeader.innerText = 'Add a new Dog'
+        newFormSection.append(newDogHeader)
 
-    //for form name
-    const form = document.createElement('form')
-    form.setAttribute('class','form')
+        //for form name
+        const form = document.createElement('form')
+        form.setAttribute('class','form')
 
-        //for new dog name 
-        const dogName = document.createElement('label')
-            dogName.setAttribute('for','name')
-            dogName.innerText ="Dog's name"
+            //for new dog name 
+            const dogName = document.createElement('label')
+                dogName.setAttribute('for','name')
+                dogName.innerText ="Dog's name"
 
-        form.append(dogName)
+            form.append(dogName)
 
-        //for input name of the dog
-        const inputName = document.createElement('input')
-            inputName.setAttribute('type','text')
-            inputName.setAttribute('id','name')
-            inputName.setAttribute('name','name')
+            //for input name of the dog
+            const inputName = document.createElement('input')
+                inputName.setAttribute('type','text')
+                inputName.setAttribute('id','name')
+                inputName.setAttribute('name','name')
 
-        form.append(inputName)
+            form.append(inputName)
 
-        //for dog image
-        const dogImage = document.createElement('label')
-            dogImage.setAttribute('for','image')
-            dogImage.innerText ="Dog's picture"
-        form.append(dogImage)
+            //for dog image
+            const dogImage = document.createElement('label')
+                dogImage.setAttribute('for','image')
+                dogImage.innerText ="Dog's picture"
+            form.append(dogImage)
+            
+            //for input image of dog
+            const inputImage = document.createElement('input')
+                inputImage.setAttribute('type','url')
+                inputImage.setAttribute('id','image')
+                inputImage.setAttribute('name','image')
+
+            form.append(inputImage)
+
+            //for dog bio
+            const dogBio = document.createElement('label')
+                dogBio.setAttribute('for','bio')
+                dogBio.innerText ="Dog's bio"
+
+            form.append(dogBio)
+
+            //for  text area
+            const textArea = document.createElement('textarea')
+                textArea.setAttribute('rows','5')
+                textArea.setAttribute('id','bio')
+                textArea.setAttribute('name','bio')
+            form.append(textArea)
+
+
+            //input submit
+            const submit = document.createElement('input')
+            submit.setAttribute('type','submit')
+            submit.setAttribute('id','submit')
+            submit.setAttribute('name','submit')
+            submit.setAttribute('value',`Let's add a dog!`)
+            submit.setAttribute('class','form__button')
+
+            form.append(submit)
+
+        newFormSection.append(form)
+
         
-        //for input image of dog
-        const inputimage = document.createElement('input')
-            inputimage.setAttribute('type','url')
-            inputimage.setAttribute('id','image')
-            inputimage.setAttribute('name','image')
+            //for the submit event of the form 
+        form.addEventListener('submit',(event) => {
+            event.preventDefault()
+        })
+        
 
-        form.append(inputimage)
+        //for the new form to display in the add button
+        const newForm = document.querySelector('form')
 
-        //for dog bio
-        const dogBio = document.createElement('label')
-            dogBio.setAttribute('for','bio')
-            dogBio.innerText ="Dog's bio"
-
-        form.append(dogBio)
-
-        //for  text area
-        const textArea = document.createElement('textarea')
-            textArea.setAttribute('rows','5')
-            textArea.setAttribute('id','bio')
-            textArea.setAttribute('name','bio')
-        form.append(textArea)
-
-
-        //input submit
-        const submit = document.createElement('input')
-        submit.setAttribute('type','submit')
-        submit.setAttribute('id','submit')
-        submit.setAttribute('name','submit')
-        submit.setAttribute('value',`Let's add a dog!`)
-        submit.setAttribute('class','form__button')
-
-        form.append(submit)
-
-        form.addEventListener('submit',event =>{
-            event.preventDefault();
-
-            const newDog =   {
-                id: 0,
-                name: "",
-                bio: ``,
+        function submitData(event)  {
+            
+            const newDogData = {
+                id: '',
+                name: event.target.name.value,
+                bio: event.target.bio.value,
                 isGoodDog: true,
-                image: ""
-              }
-            data.unshift(newDog)
-        
-            newDog.name = form.querySelector('input[id="name"]').value
-            newDog.image = form.querySelector('input[id="image"]').value
-            newDog.bio = form.querySelector('textarea[id="bio"]').value
+                image: event.target.image.value,
+            }
+            data.unshift(newDogData)
+
+            const ul = document.querySelector('ul')
+            ul.innerHTML = ''
+
+            const refreshPlusButton = document.createElement('li')
+            refreshPlusButton.setAttribute('class', 'dogs-list__button dogs-list__button--add')
+            refreshPlusButton.innerText = '+'
+            ul.append(refreshPlusButton)
+            // this is adding the plus button functionality again after first submission 
+            addDog()
+         
+
+            boom(data)
+            
+        }
+        newForm.addEventListener('submit', submitData)
 
             
-        })
-    newForm.append(form)
-})
+    })
 
+}
 
+addDog()
 
