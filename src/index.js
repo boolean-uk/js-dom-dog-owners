cdb6e5877654638e9a9b4f97dd9f90e532728ac0
Klara Andersson <klara.andersson@se.experis.com>
2024-02-19 16:43:09 +0100

    Core

--- src/index.js
+++ src/index.js
@@ -1,3 +1,136 @@
-console.log(data);
 
 // WRITE YOUR CODE BELOW!
+const dogListUl = document.querySelector('.dogs-list')
+const mainDogSection = document.querySelector('.main__dog-section')
+const addDogForum = document.querySelector('.dogs-list__button--add')
+
+function RenderDogList() {
+    for (let i = 0; i < data.length; i++) {
+        const dogList = document.createElement('li')
+        dogList.setAttribute('class', 'dogs-list__button')
+        dogList.innerText = data[i].name
+        dogList.addEventListener('click', (event => RenderMainDogCard(data[i])))
+        dogListUl.appendChild(dogList)
+    }
+}
+
+function RenderMainDogCard(dog) {        
+    mainDogSection.innerHTML = ''
+    
+    const h2 = document.createElement('h2')
+    h2.innerText = dog.name
+
+    const image = document.createElement('img')
+    image.setAttribute('src', dog.image)
+    image.setAttribute('alt', '')
+
+    const div = document.createElement('div')
+    div.setAttribute('class', 'main__dog-section__desc')
+    const h3 = document.createElement('h3')
+    h3.innerText = "Bio"
+    const pText = document.createElement('p')
+    pText.innerText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum, minima voluptates libero cumque rerum consequatur optio aliquid sint eum maxime illo laborum omnis quo ab rem cupiditate nulla perspiciatis ipsum!"
+    div.appendChild(h3)
+    div.appendChild(pText)
+
+    const pText2 = document.createElement('p')
+    const button = document.createElement('button')
+    if (dog.isGoodDog) {
+        button.innerText = 'Good dog!'
+        pText2.innerText = "Is naughty? no!"
+    }
+    else {
+        button.innerText = 'Bad dog'
+        pText2.innerText = "Is naughty? yes!"
+    }
+
+    button.addEventListener('click', (event) => {
+        dog.isGoodDog = !dog.isGoodDog
+
+        if (dog.isGoodDog) {
+            button.innerText = 'Good dog!'
+            pText2.innerText = "Is naughty? no!"
+        }
+        else {
+            button.innerText = 'Bad dog'
+            pText2.innerText = "Is naughty? yes!"
+        }
+    })
+
+    mainDogSection.appendChild(h2)
+    mainDogSection.appendChild(image)
+    mainDogSection.appendChild(div)
+    mainDogSection.appendChild(pText2)
+    mainDogSection.appendChild(button)
+}
+
+function AddForumOfDog() {
+    const h2 = document.createElement('h2')
+    const form = document.createElement('form')
+    form.setAttribute('class', 'form')
+
+    const label = document.createElement('label')
+    label.setAttribute('for', 'name')
+    label.innerText = "Dog's name"
+    const input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.setAttribute('id', 'name')
+    input.setAttribute('name', 'name')
+
+    const label2 = document.createElement('label')
+    label2.setAttribute('for', 'image')
+    label2.innerText = "Dog's picture"
+    const input2 = document.createElement('input')
+    input2.setAttribute('type', 'url')
+    input2.setAttribute('id', 'image')
+    input2.setAttribute('name', 'image')
+
+    const label3 = document.createElement('label')
+    label3.setAttribute('for', 'bio')
+    label3.innerText = "Dog's bio"
+    const textarea = document.createElement('textarea')
+    textarea.setAttribute('rows', '5')
+    textarea.setAttribute('id', 'bio')
+    textarea.setAttribute('name', 'bio')
+
+    const input3 = document.createElement('input')
+    input3.setAttribute('type', 'submit')
+    input3.setAttribute('id', 'submit')
+    input3.setAttribute('name', 'submit')
+    input3.setAttribute('value', "Let's add another dog!")
+    input3.setAttribute('class', 'form__button')
+
+    form.appendChild(label)
+    form.appendChild(input)
+    form.appendChild(label2)
+    form.appendChild(input2)
+    form.appendChild(label3)
+    form.appendChild(textarea)
+    form.appendChild(input3)
+
+    mainDogSection.appendChild(h2)
+    mainDogSection.appendChild(form)
+
+    for (let i = 0; i < data.length; i++) {
+        data[i].id += 1
+    }
+
+    const obj = 
+    {
+        id: 1,
+        name: 'Santo',
+        bio: 'Some text',
+        isGoodDog: true,
+        image: 'https://www.google.com/search?safe=active&sca_esv=1ba2041d0738a613&rlz=1C1GCEU_enSE1091SE1091&sxsrf=ACQVn09uHYUBhR1DVX06yUhaTZyuq1-brw:1708356276156&q=aussies&tbm=isch&source=lnms&sa=X&ved=2ahUKEwj5t_WH27eEAxVdAxAIHVR6DaoQ0pQJegQIDhAB&biw=1280&bih=593&dpr=1.5#imgrc=hJUus9orcmgDZM'
+    }
+    data.unshift(obj)
+    dogListUl.innerHTML = ''
+    RenderDogList()
+}
+
+function main() {
+    RenderDogList()
+    addDogForum.addEventListener('click', (event) => AddForumOfDog())
+}
+
+main()
