f2ffc3e8239dc119202fe74591fd6d44c3b05824
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-03-26 13:28:04 +0100

    add dog list item function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 console.log(data);
+const dogList = document.querySelector('.dogs-list')
+
 
 // WRITE YOUR CODE BELOW!
+function addDogListItem() {
+    for (let i = 0; i < data.length; i++) {
+        const newDogListItem = document.createElement('li')
+        newDogListItem.classList.add('dogs-list__button')
+        newDogListItem.innerText = data[i].name
+        dogList.append(newDogListItem)
+
+        const dogListItem = document.querySelectorAll('.dogs-list__button')
+    }
+    return dogListItem
+}
+
+addDogListItem()
+const dogsListItem = addDogListItem()
+console.log(dogsListItem)
