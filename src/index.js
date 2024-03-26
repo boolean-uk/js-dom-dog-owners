d4a02b58fdfc46a5e6b1faef9a3d80910d3d522d
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-03-26 15:15:14 +0100

    add good boy functions and finish main dog section function

--- src/index.js
+++ src/index.js
@@ -24,11 +24,28 @@ addDogListItem()
 function dogCard(dog) {
     const newSection = createDogSection()
     const dogName = document.createElement('h2')
+    const image = document.createElement('img')
+    const mainDogSection = createMainDogSection()
+    const naughty = document.createElement('p')
+    const em = document.createElement('em')
+    const button = goodDogButton(dog)
+    
     main.innerHTML = ''
     
     dogName.innerText = dog.name
 
+    image.setAttribute('src', dog.image)
+    image.setAttribute('alt', " ")
+
+    naughty.innerText = isGoodBoy(dog)
+    em.innerText = 'Is naughty? '
+
     newSection.append(dogName)
+    newSection.append(image)
+    newSection.append(mainDogSection)
+    newSection.append(naughty)
+    naughty.prepend(em)
+    newSection.append(button)
 
     main.append(newSection)
 }
@@ -38,4 +55,44 @@ function createDogSection() {
     newSection.classList.add('main__dog-section')
 
     return newSection
+}
+
+function createMainDogSection() {
+    const mainDogSection = document.createElement('div')
+    const bio = document.createElement('h3')
+    const p = document.createElement('p')
+
+    bio.innerText = 'Bio'
+
+    mainDogSection.classList.add('main__dog-section__desc')
+
+    p.innerText = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum,
+    minima voluptates libero cumque rerum consequatur optio aliquid sint
+    eum maxime illo laborum omnis quo ab rem cupiditate nulla
+    perspiciatis ipsum!`
+
+    mainDogSection.append(bio)
+    mainDogSection.append(p)
+
+    return mainDogSection
+}
+
+function isGoodBoy(dog) {
+    if (dog.isGoodDog) {
+        return 'no!'
+    }
+    return 'yes!'
+}
+
+function isGoodDogButton(dog) {
+    if (dog.isGoodDog) {
+        return 'Bad dog!'
+    }
+    return 'Good dog!'
+}
+
+function goodDogButton(dog) {
+    const button = document.createElement('button')
+    button.innerText = isGoodDogButton(dog)
+    return button
 }
