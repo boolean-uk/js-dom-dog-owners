aa04b31667da1ebdf7083f177c930f624e08433f
Tim Zoltie <t.zoltie@gmail.com>
2024-03-27 19:14:39 +0000

    refactoring_organised_into_functions

--- src/index.js
+++ src/index.js
@@ -6,29 +6,51 @@ const dog = data
 const dogList = document.querySelector('.dogs-list')
 const addDogButton = document.querySelector('.dogs-list__button--add')
 const dogHeaderButton = document.querySelector('.dogs-list__button')
+const mainSection = document.querySelector('.main')
 const cardSection = document.querySelector('.main__dog-section')
 
 
 
-for (let i = 0; i < dog.length; i++) {
-    const li = document.createElement('li')
-    li.classList.add('dogs-list__button')
-    dogList.append(li)
+function createDogButtons() {
+    for (let i = 0; i < dog.length; i++) {
     
-    const headerH2 = document.createElement('h2')
-    headerH2.innerText = dog[i].name
-    li.append(headerH2)
+        const li = document.createElement('li')
+        addDogButton.addEventListener('click', newDogForm)
+        
+
+        li.classList.add('dogs-list__button')
+        dogList.append(li)
+            
+        const headerH2 = document.createElement('h2')
+        headerH2.innerText = dog[i].name
+        headerH2.addEventListener('click', () => createDogCard(headerH2))
+        li.append(headerH2)
+        
+        
+    }
+    
+}
+ 
+createDogButtons()
 
 
-    headerH2.addEventListener('click', () => {
+
+function createDogCard(headerH2) {
+
+    
+    
+    dog.forEach((headerH2) => {
+
         cardSection.innerHTML = ''
+        
         const cardH2 = document.createElement('h2')
-        cardH2.innerText = dog[i].name
+        cardH2.innerText = headerH2.name
         cardSection.append(cardH2)
 
         const cardImage = document.createElement('img')
-        cardImage.setAttribute('src', dog[i].image)
+        cardImage.setAttribute('src', headerH2.image)
         cardSection.append(cardImage)
+        
     
         const div = document.createElement('div')
         div.classList.add('main__dog-section__desc')
@@ -38,34 +60,38 @@ for (let i = 0; i < dog.length; i++) {
         div.append(cardH3)
     
         const bio = document.createElement('p')
-        bio.innerText = ('Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum')
+        bio.innerText = ('src', headerH2.bio)
         div.append(bio)
     
         const goodOrBadElement = document.createElement('p')
-        cardSection.append(goodOrBadElement)
-    
+        
+        
         const italics = document.createElement('em')
         italics.innerText = ('Is naughty?')
         goodOrBadElement.append(italics)
 
+        
         const goodOrBadButton = document.createElement('button')
-        const goodOrBad = dog[i].isGoodDog
+        
+        cardSection.append(goodOrBadElement)
+        cardSection.append(goodOrBadButton)
+        
+        const goodOrBad = headerH2.isGoodDog
         if(goodOrBad === true) {
             goodOrBadElement.innerText = ('No!')
             goodOrBadButton.innerText = ('Good Dog')}
             else {
                 goodOrBadElement.innerText = ('Yes!')
                 goodOrBadButton.innerText = ('Bad Dog')}
-        cardSection.append(goodOrBadButton)
-
     
-        
-    })
+    })    
+}
+ 
 
-    addDogButton.addEventListener('click', () => {
 
-        cardSection.innerHTML = ''
-        const newDogHeading = document.createElement('h2')
+function newDogForm() {
+    cardSection.innerHTML = ''
+    const newDogHeading = document.createElement('h2')
         newDogHeading.innerText = ('Add a new Dog')
         cardSection.append(newDogHeading)
 
@@ -116,16 +142,18 @@ for (let i = 0; i < dog.length; i++) {
 
         newDogForm.addEventListener('submit', () => {
             event.preventDefault()
-
+            
+            cardSection.innerHTML = ''
             const li = document.createElement('li')
             li.classList.add('dogs-list__button')
             dogList.append(li)
-
+        
             const headerH2 = document.createElement('h2')
             headerH2.innerText = `${dogNameInput.value}`
             li.append(headerH2)
             
+            
         })
-    })
 }
-    
+
+
